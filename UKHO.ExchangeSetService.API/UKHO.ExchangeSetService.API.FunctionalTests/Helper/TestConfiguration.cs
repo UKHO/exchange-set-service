6234f10ee248f3ff54bd6b08245dc8a7eaa88c62
Mrunmayee Godse <Mrunmay14436@Mastek.Com>
2021-05-07 13:43:01 +0530

    2730 - Resolved review comments AB#2730 AB#6799

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/Helper/TestConfiguration.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/Helper/TestConfiguration.cs
@@ -3,23 +3,11 @@ using Microsoft.Extensions.Configuration;
 
 namespace UKHO.ExchangeSetService.API.FunctionalTests.Helper
 {
-    /// <summary>
-    /// This class is for configuration set up
-    /// </summary>
     public class TestConfiguration
     {
-        /// <summary>
-        /// ConfigurationRoot variable declaire here 
-        /// </summary>
-        protected IConfigurationRoot ConfigurationRoot;
-        /// <summary>
-        /// EssBaseAddress variable declaire here 
-        /// </summary>
-        public string EssBaseAddress;
 
-        /// <summary>
-        /// Constructor call here
-        /// </summary>
+        protected IConfigurationRoot ConfigurationRoot;
+        public string EssBaseAddress;
         public TestConfiguration()
         {
             ConfigurationRoot = new ConfigurationBuilder()
