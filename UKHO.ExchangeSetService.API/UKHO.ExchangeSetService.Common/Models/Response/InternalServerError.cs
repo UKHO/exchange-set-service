748db406045ab9eccbab87376d11e80de76c7dcc
Siddartha Mote <siddart14393@mastek.com>
2021-05-07 11:39:58 +0530

    2729-changes made related to InternalServerError class from Details to detail #2729 #6799

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Models/Response/InternalServerError.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Models/Response/InternalServerError.cs
@@ -6,7 +6,7 @@ namespace UKHO.ExchangeSetService.Common.Models.Response
     public class InternalServerError
     {
         public string CorrelationId { get; set; }
-        public string Details { get; set; }
+        public string Detail { get; set; }
     }
 
 }
