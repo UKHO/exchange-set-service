843bb167e543075288811423042251c5f4026901
ravi102465 <ravi102465@mastek.com>
2021-05-10 11:40:37 +0530

    AB#2855 CR and improvement around terraform scripts

--- Deployment/terraform_conditional_run.ps1
+++ Deployment/terraform_conditional_run.ps1
@@ -1,15 +1,15 @@
 param (
-    [Parameter(Mandatory = $true)] [string] $DeploymentResourceGroupName,
-    [Parameter(Mandatory = $true)] [string] $DeploymentStorageAccountName,
-    [Parameter(Mandatory = $true)] [string] $WorkSpace,
-    [Parameter(Mandatory = $true)] [string] $Terraformapply
+    [Parameter(Mandatory = $true)] [string] $deploymentResourceGroupName,
+    [Parameter(Mandatory = $true)] [string] $deploymentStorageAccountName,
+    [Parameter(Mandatory = $true)] [string] $workSpace,
+    [Parameter(Mandatory = $true)] [boolean] $continueEvenIfResourcesAreGettingDestroyed
 )
 
 cd $env:AGENT_BUILDDIRECTORY/terraformartifact/src
 
-Write-output "Executing terraform scripts for deployment in $WorkSpace Enviroment"
-terraform init -backend-config="resource_group_name=$DeploymentResourceGroupName" -backend-config="storage_account_name=$DeploymentStorageAccountName" -backend-config="key=terraform.deployment.tfplan"
-if ( !$? ) { echo "something went wrong during terraform initialization"; throw "Error" }
+Write-output "Executing terraform scripts for deployment in $workSpace enviroment"
+terraform init -backend-config="resource_group_name=$deploymentResourceGroupName" -backend-config="storage_account_name=$deploymentStorageAccountName" -backend-config="key=terraform.deployment.tfplan"
+if ( !$? ) { echo "Something went wrong during terraform initialization"; throw "Error" }
 
 Write-output "Selecting workspace"
 
@@ -17,42 +17,36 @@ $ErrorActionPreference = 'SilentlyContinue'
 terraform workspace new $WorkSpace 2>&1 > $null
 $ErrorActionPreference = 'Continue'
 
-terraform workspace select $WorkSpace
-if ( !$? ) { echo "Error while selecting workspace trying to create it"; throw "Error" }
+terraform workspace select $workSpace
+if ( !$? ) { echo "Error while selecting workspace"; throw "Error" }
 
 Write-output "Validating terraform"
 terraform validate
-if ( !$? ) { echo "something went wrong during terraform validation" ; throw "Error" }
+if ( !$? ) { echo "Something went wrong during terraform validation" ; throw "Error" }
 
 Write-output "Execute Terraform plan"
 terraform plan -out "terraform.deployment.tfplan" -no-color | tee terraform_output.txt
-if ( !$? ) { echo "something went wrong during terraform plan" ; throw "Error" }
+if ( !$? ) { echo "Something went wrong during terraform plan" ; throw "Error" }
 
-$totaldestroylines=(Get-Content -Path terraform_output.txt | Select-String -Pattern "destroy" -CaseSensitive |  where {$_ -ne ""}).length
-   if($totaldestroylines -ge 2) {
-      write-Host("terraform is destroying some resources, please Verify...................")
-      $destroyingresources = "true"
-      Write-Host "##vso[task.setvariable variable=DestroyResource]$destroyingresources"
-         if ( $Terraformapply -ceq "False" ) {
-            write-Host("exiting...................")
-            Write-Output $_
-            exit 1
-         else {
-            write-host("Continue executing terraform apply")
-            $destroyingresources = "false"
-            Write-Host "##vso[task.setvariable variable=DestroyResource]$destroyingresources"
-         }
-      }
-   }
+$totalDestroyLines=(Get-Content -Path terraform_output.txt | Select-String -Pattern "destroy" -CaseSensitive |  where {$_ -ne ""}).length
+if($totalDestroyLines -ge 2) 
+{
+    write-Host("Terraform is destroying some resources, please verify...................")
+    if ( !$ContinueEvenIfResourcesAreGettingDestroyed) 
+    {
+        write-Host("exiting...................")
+        Write-Output $_
+        exit 1
+    }
+    write-host("Continue executing terraform apply - as continueEvenIfResourcesAreGettingDestroyed param is set to true in pipeline")
+}
 
 Write-output "Executing terraform apply"
 terraform apply  "terraform.deployment.tfplan"
-if ( !$? ) { echo "something went wrong during terraform apply" ; throw "Error" }
+if ( !$? ) { echo "Something went wrong during terraform apply" ; throw "Error" }
 
-Write-output "terraform output as json"
+Write-output "Terraform output as json"
 $terraformOutput = terraform output -json | ConvertFrom-Json
 
-echo $terraformOutput
-
-write-output "Set JSON output into Pipeline Variables"
+write-output "Set JSON output into pipeline variables"
 Write-Host "##vso[task.setvariable variable=WEB_APP_NAME]$($terraformOutput.web_app_name.value)"
--- Deployment/terraform_run.ps1
+++ Deployment/terraform_run.ps1
@@ -1,6 +1,6 @@
 & cls
-& terraform init -backend-config="resource_group_name=FileShareSerDevP4052-RG" -backend-config="storage_account_name=fileshareserdevp4052" -backend-config="key=terraform.deployment.tfplan"
+& terraform init -backend-config="resource_group_name=" -backend-config="storage_account_name=" -backend-config="key=terraform.deployment.tfplan"
 & terraform workspace select dev 
 & terraform validate
-& terraform plan -var 'resource_group_name=FileShareService' -out "terraform.deployment.tfplan"
+& terraform plan -out "terraform.deployment.tfplan"
 #& terraform apply terraform.deployment.tfplan
