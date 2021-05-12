03c811bd7f2fe05287e9a44b461d7bc66ac70f1c
Ravi102465 <Ravi102465@mastek.com>
2021-05-12 20:28:59 +0530

    AB#2855 - changes for terraform and service connection

--- Deployment/src/azure.tf
+++ Deployment/src/azure.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 
-  required_version = "=0.14.4"
+  required_version = "=0.14.9"
   backend "azurerm" {
     container_name = "tfstate"
     key            = "terraform.deployment.tfplan"
