0325b25187ebdfcb2cd9f753d5d46de818df271f
Siddartha Mote <siddart14393@mastek.com>
2021-05-21 12:05:30 +0530

    6850 changes related to code coverage with other review comments AB#6850 AB#6849 AB#7107

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Mappings/AutoMapperProfile.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Mappings/AutoMapperProfile.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
+using System.Diagnostics.CodeAnalysis;
 using UKHO.ExchangeSetService.Common.Models.Response;
 using UKHO.ExchangeSetService.Common.Models.SalesCatalogue;
 
 namespace UKHO.ExchangeSetService.API.Mappings
 {
+    [ExcludeFromCodeCoverage] ////Excluded from code coverage as it used in startup
     public class AutoMapperProfile : Profile
     {
         public AutoMapperProfile()
