2766f557c6d6553f380ef1d4975dcfe6756b6c66
Ravi102465 <Ravi102465@mastek.com>
2021-06-07 14:13:43 +0530

    AB#2605 - Removed comment for network rules

--- Deployment/src/Modules/Webapp/main.tf
+++ Deployment/src/Modules/Webapp/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_app_service" "webapp_service" {
   https_only = true
 }
 
-/*resource "azurerm_app_service_virtual_network_swift_connection" "webapp_vnet_integration" {
+resource "azurerm_app_service_virtual_network_swift_connection" "webapp_vnet_integration" {
   app_service_id = azurerm_app_service.webapp_service.id
   subnet_id      = var.subnet_id
-}*/
+}
