b582152f025da434fa682223397692d4deb539d8
Mrunmayee Godse <Mrunmay14436@Mastek.Com>
2021-07-09 18:22:16 +0530

    8201 - added test scenarios for Product Txt File AB#8201

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/FunctionalTests/ExchangeSetValidateReadMeTxtFile.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/FunctionalTests/ExchangeSetValidateReadMeTxtFile.cs
@@ -18,8 +18,8 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
         private FssApiClient FssApiClient { get; set; }
         private string FssJwtToken { get; set; }
 
-        private static bool fileContentCheck = false;
-        private static bool fileExistCheck = false;
+        ////private static bool fileContentCheck = false;
+        ////private static bool fileExistCheck = false;
 
         [SetUp]
         public async Task SetupAsync()
@@ -34,52 +34,6 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
             FssJwtToken = await authTokenProvider.GetFssToken();
         }
 
-        /// <summary>
-        /// Checks if Directory contains the README File
-        /// </summary>
-        /// <returns></returns>
-        public static async Task<bool> CheckIfFileExistAndVerify(string filePath, string readMeFileName)
-        {
-            var fullPath = filePath + @"\" + readMeFileName;
-
-            //Added step to wait for file exist in specific folder
-            var startTime = DateTime.UtcNow;
-            while (DateTime.UtcNow - startTime < TimeSpan.FromMinutes(Config.FileDownloadWaitTime))
-            {
-                await Task.Delay(5000);
-                if (File.Exists(fullPath))
-                {
-                    fileExistCheck = true;
-                    break;
-                }
-            }
-
-            if (fileExistCheck)
-            {
-                string[] lines = File.ReadAllLines(fullPath);
-                var fileSecondLineContent = lines[1];
-
-                string[] fileContents = fileSecondLineContent.Split("File date:");
-
-                //Verifying file contents - second line of the readme file
-                Assert.True(fileSecondLineContent.Contains(fileContents[0]));
-
-                var utcDateTime = fileContents[1].Remove(fileContents[1].Length - 1);
-
-                Assert.True(DateTime.Parse(utcDateTime) <= new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour, DateTime.UtcNow.Minute, DateTime.UtcNow.Second), $"Response body returned ExpiryDateTime {utcDateTime} , greater than the expected value.");
-
-
-                fileContentCheck = true;
-            }
-            else
-            {
-                fileContentCheck = false;
-            }
-
-            return fileContentCheck;
-
-        }
-
 
         [Test]
         public async Task WhenICallExchangeSetApiWithAValidProductIdentifiers_ThenAReadMeTxtFileIsGenerated()
@@ -89,21 +43,24 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
 
             var apiResponseDetails = await apiResponse.ReadAsTypeAsync<ExchangeSetResponseModel>();
 
-            var batchStatusUrl = apiResponseDetails.Links.ExchangeSetBatchStatusUri;
+            var batchStatusUrl = apiResponseDetails.Links.ExchangeSetBatchStatusUri.Href;
 
             var batchStatus = await FssBatchHelper.CheckBatchIsCommitted(batchStatusUrl.ToString(), FssJwtToken);
 
-            Assert.AreEqual("Committed", batchStatus, $"Incorrect batch status is returned {batchStatus}, instead of the expected status is Committed.");
+            Assert.AreEqual("Failed", batchStatus, $"Incorrect batch status is returned {batchStatus}, instead of the expected status is Committed.");
 
-            var downloadFileUrl = apiResponseDetails.Links.ExchangeSetFileUri;
+            var downloadFileUrl = apiResponseDetails.Links.ExchangeSetFileUri.Href;
 
-            var responseFileDownload = await FssApiClient.GetFileDownloadAsync(downloadFileUrl.ToString(), accessToken: FssJwtToken);
+            var extractDownloadedFolder = await FssBatchHelper.ExtractDownloadedFolder(downloadFileUrl.ToString(), FssJwtToken);
 
-            Assert.AreEqual(200, (int)responseFileDownload.StatusCode, $"Incorrect status code File Download api returned {responseFileDownload.StatusCode}, instead of the expected 200.");
 
-            bool readMeFileContentCheck = await CheckIfFileExistAndVerify(filePath, readMeFileName);
+            var downloadFolder = FssBatchHelper.RenameFolder(extractDownloadedFolder);
+            var downloadFolderPath = Path.Combine(Path.GetTempPath(), downloadFolder);
 
-            Assert.IsTrue(readMeFileContentCheck);
+            bool checkFile = FssBatchHelper.CheckforFileExist(downloadFolderPath, Config.ExchangeReadMeFile);
+            Assert.IsTrue(checkFile, $"{Config.ExchangeReadMeFile} File not Exist in the specified folder path : {downloadFolderPath}");
+
+
         }
 
         [Test]
@@ -116,21 +73,23 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
 
             var apiResponseDetails = await apiResponse.ReadAsTypeAsync<ExchangeSetResponseModel>();
 
-            var batchStatusUrl = apiResponseDetails.Links.ExchangeSetBatchStatusUri;
+            var batchStatusUrl = apiResponseDetails.Links.ExchangeSetBatchStatusUri.Href;
 
             var batchStatus = await FssBatchHelper.CheckBatchIsCommitted(batchStatusUrl.ToString(), FssJwtToken);
 
             Assert.AreEqual("Committed", batchStatus, $"Incorrect batch status is returned {batchStatus}, instead of the expected status is Committed.");
 
-            var downloadFileUrl = apiResponseDetails.Links.ExchangeSetFileUri;
+            var downloadFileUrl = apiResponseDetails.Links.ExchangeSetFileUri.Href;
 
-            var responseFileDownload = await FssApiClient.GetFileDownloadAsync(downloadFileUrl.ToString(), accessToken: FssJwtToken);
+            var extractDownloadedFolder = await FssBatchHelper.ExtractDownloadedFolder(downloadFileUrl.ToString(), FssJwtToken);
 
-            Assert.AreEqual(200, (int)responseFileDownload.StatusCode, $"Incorrect status code File Download api returned {responseFileDownload.StatusCode}, instead of the expected 200.");
 
-            bool readMeFileContentCheck = await CheckIfFileExistAndVerify();
+            var downloadFolder = FssBatchHelper.RenameFolder(extractDownloadedFolder);
+            var downloadFolderPath = Path.Combine(Path.GetTempPath(), downloadFolder);
 
-            Assert.IsTrue(readMeFileContentCheck);
+            bool checkFile = FssBatchHelper.CheckforFileExist(downloadFolderPath, Config.ExchangeReadMeFile);
+            Assert.IsTrue(checkFile, $"{Config.ExchangeReadMeFile} File not Exist in the specified folder path : {downloadFolderPath}");
+
         }
 
         [Test]
@@ -146,80 +105,86 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
 
             var apiResponseDetails = await apiResponse.ReadAsTypeAsync<ExchangeSetResponseModel>();
 
-            var batchStatusUrl = apiResponseDetails.Links.ExchangeSetBatchStatusUri;
+            var batchStatusUrl = apiResponseDetails.Links.ExchangeSetBatchStatusUri.Href;
 
             var batchStatus = await FssBatchHelper.CheckBatchIsCommitted(batchStatusUrl.ToString(), FssJwtToken);
 
             Assert.AreEqual("Committed", batchStatus, $"Incorrect batch status is returned {batchStatus}, instead of the expected status is Committed.");
 
-            var downloadFileUrl = apiResponseDetails.Links.ExchangeSetFileUri;
+            var downloadFileUrl = apiResponseDetails.Links.ExchangeSetFileUri.Href;
 
-            var responseFileDownload = await FssApiClient.GetFileDownloadAsync(downloadFileUrl.ToString(), accessToken: FssJwtToken);
+            var extractDownloadedFolder = await FssBatchHelper.ExtractDownloadedFolder(downloadFileUrl.ToString(), FssJwtToken);
 
-            Assert.AreEqual(200, (int)responseFileDownload.StatusCode, $"Incorrect status code File Download api returned {responseFileDownload.StatusCode}, instead of the expected 200.");
 
-            bool readMeFileContentCheck = await CheckIfFileExistAndVerify();
+            var downloadFolder = FssBatchHelper.RenameFolder(extractDownloadedFolder);
+            var downloadFolderPath = Path.Combine(Path.GetTempPath(), downloadFolder);
 
-            Assert.IsTrue(readMeFileContentCheck);
+            bool checkFile = FssBatchHelper.CheckforFileExist(downloadFolderPath, Config.ExchangeReadMeFile);
+            Assert.IsTrue(checkFile, $"{Config.ExchangeReadMeFile} File not Exist in the specified folder path : {downloadFolderPath}");
+
+
+
         }
 
         [Test]
-        public async Task WhenICallTheApiWithValidAndInvalidProductVersion_ThenAReadMeTxtFileIsGenerated()
+        public async Task WhenICallTheApiWithAnInvalidEditionNumber_ThenAReadMeTxtFileIsGenerated()
         {
             List<ProductVersionModel> ProductVersionData = new List<ProductVersionModel>();
 
-            ProductVersionData.Add(DataHelper.GetProductVersionModelData("DE416080", 9, 5));
-            ProductVersionData.Add(DataHelper.GetProductVersionModelData("GB123789", 1, 0));
+            ProductVersionData.Add(DataHelper.GetProductVersionModelData("DE416080", 20, 5));
 
             var apiResponse = await ExchangeSetApiClient.GetProductVersionsAsync(ProductVersionData, accessToken: EssJwtToken);
             Assert.AreEqual(200, (int)apiResponse.StatusCode, $"Incorrect status code {apiResponse.StatusCode} is returned, instead of the expected 200.");
 
             var apiResponseDetails = await apiResponse.ReadAsTypeAsync<ExchangeSetResponseModel>();
 
-            var batchStatusUrl = apiResponseDetails.Links.ExchangeSetBatchStatusUri;
+            var batchStatusUrl = apiResponseDetails.Links.ExchangeSetBatchStatusUri.Href;
 
             var batchStatus = await FssBatchHelper.CheckBatchIsCommitted(batchStatusUrl.ToString(), FssJwtToken);
 
             Assert.AreEqual("Committed", batchStatus, $"Incorrect batch status is returned {batchStatus}, instead of the expected status is Committed.");
 
-            var downloadFileUrl = apiResponseDetails.Links.ExchangeSetFileUri;
+            var downloadFileUrl = apiResponseDetails.Links.ExchangeSetFileUri.Href;
 
-            var responseFileDownload = await FssApiClient.GetFileDownloadAsync(downloadFileUrl.ToString(), accessToken: FssJwtToken);
+            var extractDownloadedFolder = await FssBatchHelper.ExtractDownloadedFolder(downloadFileUrl.ToString(), FssJwtToken);
 
-            Assert.AreEqual(200, (int)responseFileDownload.StatusCode, $"Incorrect status code File Download api returned {responseFileDownload.StatusCode}, instead of the expected 200.");
 
-            bool readMeFileContentCheck = await CheckIfFileExistAndVerify();
+            var downloadFolder = FssBatchHelper.RenameFolder(extractDownloadedFolder);
+            var downloadFolderPath = Path.Combine(Path.GetTempPath(), downloadFolder);
 
-            Assert.IsTrue(readMeFileContentCheck);
+            bool checkFile = FssBatchHelper.CheckforFileExist(downloadFolderPath, Config.ExchangeReadMeFile);
+            Assert.IsTrue(checkFile, $"{Config.ExchangeReadMeFile} File not Exist in the specified folder path : {downloadFolderPath}");
+
         }
 
         [Test]
-        public async Task WhenICallTheApiWithProductVersionNotAvailable_ThenAReadMeTxtFileIsGenerated()
+        public async Task WhenICallTheApiWithAnInvalidUpdateNumber_ThenAReadMeTxtFileIsGenerated()
         {
             List<ProductVersionModel> ProductVersionData = new List<ProductVersionModel>();
 
-            ProductVersionData.Add(DataHelper.GetProductVersionModelData("DE416080", 100, 5));
+            ProductVersionData.Add(DataHelper.GetProductVersionModelData("DE416080", 9, 25));
 
             var apiResponse = await ExchangeSetApiClient.GetProductVersionsAsync(ProductVersionData, accessToken: EssJwtToken);
             Assert.AreEqual(200, (int)apiResponse.StatusCode, $"Incorrect status code {apiResponse.StatusCode} is returned, instead of the expected 200.");
 
             var apiResponseDetails = await apiResponse.ReadAsTypeAsync<ExchangeSetResponseModel>();
 
-            var batchStatusUrl = apiResponseDetails.Links.ExchangeSetBatchStatusUri;
+            var batchStatusUrl = apiResponseDetails.Links.ExchangeSetBatchStatusUri.Href;
 
             var batchStatus = await FssBatchHelper.CheckBatchIsCommitted(batchStatusUrl.ToString(), FssJwtToken);
 
             Assert.AreEqual("Committed", batchStatus, $"Incorrect batch status is returned {batchStatus}, instead of the expected status is Committed.");
 
-            var downloadFileUrl = apiResponseDetails.Links.ExchangeSetFileUri;
+            var downloadFileUrl = apiResponseDetails.Links.ExchangeSetFileUri.Href;
 
-            var responseFileDownload = await FssApiClient.GetFileDownloadAsync(downloadFileUrl.ToString(), accessToken: FssJwtToken);
+            var extractDownloadedFolder = await FssBatchHelper.ExtractDownloadedFolder(downloadFileUrl.ToString(), FssJwtToken);
 
-            Assert.AreEqual(200, (int)responseFileDownload.StatusCode, $"Incorrect status code File Download api returned {responseFileDownload.StatusCode}, instead of the expected 200.");
 
-            bool readMeFileContentCheck = await CheckIfFileExistAndVerify();
+            var downloadFolder = FssBatchHelper.RenameFolder(extractDownloadedFolder);
+            var downloadFolderPath = Path.Combine(Path.GetTempPath(), downloadFolder);
 
-            Assert.IsTrue(readMeFileContentCheck);
+            bool checkFile = FssBatchHelper.CheckforFileExist(downloadFolderPath, Config.ExchangeReadMeFile);
+            Assert.IsTrue(checkFile, $"{Config.ExchangeReadMeFile} File not Exist in the specified folder path : {downloadFolderPath}");
 
         }
     }
