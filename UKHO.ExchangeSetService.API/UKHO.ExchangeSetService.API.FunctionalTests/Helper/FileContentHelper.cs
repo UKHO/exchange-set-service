85274363d92e951d477c48479a4c95085137db93
Mrunmayee Godse <Mrunmay14436@Mastek.Com>
2021-07-12 21:31:16 +0530

    8201 - Added the code for ReadMe.TXT file content verification AB#8201 AB#8297

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/Helper/FileContentHelper.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/Helper/FileContentHelper.cs
@@ -21,16 +21,35 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.Helper
             string formatVersionAndExchangeSetNumber = fileContent[7];
 
             int weekNumber = CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(DateTime.UtcNow, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
-            string year = DateTime.UtcNow.Year.ToString().Substring(DateTime.UtcNow.Year.ToString().Length-2);
+            string year = DateTime.UtcNow.Year.ToString().Substring(DateTime.UtcNow.Year.ToString().Length - 2);
             string currentDate = DateTime.UtcNow.ToString("yyyyMMdd");
 
             Assert.AreEqual(dataServerAndWeek, $"GBWK{weekNumber}-{year}");
 
-            Assert.AreEqual(dateAndCdType,$"{currentDate}UPDATE");
+            Assert.AreEqual(dateAndCdType, $"{currentDate}UPDATE");
 
             Assert.IsTrue(formatVersionAndExchangeSetNumber.StartsWith("02.00U01X01"), $"Expected format version {formatVersionAndExchangeSetNumber}");
 
         }
 
+        public static void CheckReadMeTxtFileContent(string inputfile)
+        {
+
+            string[] lines = File.ReadAllLines(inputfile);
+            var fileSecondLineContent = lines[1];
+
+            string[] fileContents = fileSecondLineContent.Split("File date:");
+
+            //Verifying file contents - second line of the readme file
+            Assert.True(fileSecondLineContent.Contains(fileContents[0]));
+
+            var utcDateTime = fileContents[1].Remove(fileContents[1].Length - 1);
+
+            Assert.True(DateTime.Parse(utcDateTime) <= new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, DateTime.UtcNow.Day, DateTime.UtcNow.Hour, DateTime.UtcNow.Minute, DateTime.UtcNow.Second), $"Response body returned ExpiryDateTime {utcDateTime} , greater than the expected value.");
+        }
+
     }
+
+
 }
+
