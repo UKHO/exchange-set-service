8fa089370af67ddf49c3906549d9d073241c63a1
Ravi102465 <Ravi102465@mastek.com>
2021-07-20 20:26:20 +0530

    Added service check script

---
+++ Deployment/check_service_status.ps1
@@ -0,0 +1,55 @@
+Param(
+	[Parameter(mandatory=$true)][string]$healthEndPointUrl,
+    [Parameter(mandatory=$true)][string]$waitTimeInMinute
+)
+
+$sleepTimeInSecond = 20
+$isServiceActive = 'false'
+
+$stopWatch = New-Object -TypeName System.Diagnostics.Stopwatch
+$timeSpan = New-TimeSpan -Minutes $waitTimeInMinute
+$stopWatch.Start()
+
+do
+{
+    Write-Host "Polling url: $healthEndPointUrl..."
+    try{
+        Start-Sleep -Seconds $sleepTimeInSecond
+        $HttpRequest  = [System.Net.WebRequest]::Create("$healthEndPointUrl")
+        $HttpResponse = $HttpRequest.GetResponse() 
+        $HttpStatus   = $HttpResponse.StatusCode
+        Write-Host "Status code of web is $HttpStatus..."
+    
+        If ($HttpStatus -eq 200 ) {
+            Write-Host "Service is up. Stopping Polling..."
+            $isServiceActive = 'true'
+            break
+        }
+        Else {
+            Write-Host "Service not yet Up. Status code: $HttpStatus..."
+            Start-Sleep -Seconds $sleepTimeInSecond
+        }
+    }
+    catch [System.Net.WebException]
+    {
+        $HttpStatus = $_.Exception.Response.StatusCode.Value__
+        Write-Host "Service not yet Up. Status code: $HttpStatus..."
+        Start-Sleep -Seconds $sleepTimeInSecond
+    }
+    
+    
+}
+until ($stopWatch.Elapsed -ge $timeSpan)
+
+
+If ($HttpResponse -ne $null) { 
+    $HttpResponse.Close() 
+}
+
+if ($isServiceActive -eq 'true' ) {
+    Write-Host "Service is up returnig from script..."
+}
+Else { 
+    Write-Error "Service was not up in $waitTimeInMinute, error while deployment..."
+    throw "Error"
+}
