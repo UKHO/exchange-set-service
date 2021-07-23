c28e51dc72e90c802b0fcb8f26e0172673e45dc0
Shailendra Shinde <shailen14599@mastek.com>
2021-07-23 10:16:50 +0530

    7206 - Added code changes for creating error.txt file AB#7206 AB#8705

---
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Configuration/CustomException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace UKHO.ExchangeSetService.Common.Configuration
+{
+    [Serializable]
+    public class CustomException : Exception
+    {
+        private static readonly string message = "There has been a problem in creating your exchange set, so we are unable to fulfil your request at this time. " +
+            "Please contact UKHO Customer Services quoting error code XXX and correlation ID : correlationId";
+
+        public string CorrelationId { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public CustomException()
+        {
+        }
+
+        public CustomException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public CustomException(string correlationId) : base(message)
+        {
+            CorrelationId = correlationId;
+            ErrorMessage = message.Replace("correlationId", correlationId);
+        }
+
+        protected CustomException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.FulfilmentService/FulfilmentServiceJob.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.FulfilmentService/FulfilmentServiceJob.cs
@@ -3,8 +3,12 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Threading.Tasks;
+using UKHO.ExchangeSetService.Common.Configuration;
+using UKHO.ExchangeSetService.Common.Helpers;
 using UKHO.ExchangeSetService.Common.Logging;
 using UKHO.ExchangeSetService.Common.Models.SalesCatalogue;
 using UKHO.ExchangeSetService.FulfilmentService.Services;
@@ -13,25 +17,43 @@ namespace UKHO.ExchangeSetService.FulfilmentService
     [ExcludeFromCodeCoverage]
     public class FulfilmentServiceJob
     {
-        protected IConfiguration ConfigurationBuilder;
+        protected IConfiguration configuration;
         private readonly IFulfilmentDataService fulFilmentDataService;
         private readonly ILogger<FulfilmentServiceJob> logger;
+        private readonly IFileSystemHelper fileSystemHelper;
 
         public FulfilmentServiceJob(IConfiguration configuration,
-                                    IFulfilmentDataService fulFilmentDataService, ILogger<FulfilmentServiceJob> logger)
+                                    IFulfilmentDataService fulFilmentDataService, ILogger<FulfilmentServiceJob> logger, IFileSystemHelper fileSystemHelper)
         {
+            this.configuration = configuration;
             this.fulFilmentDataService = fulFilmentDataService;
             this.logger = logger;
+            this.fileSystemHelper = fileSystemHelper;
         }
+
         public async Task ProcessQueueMessage([QueueTrigger("%QueueName%")] CloudQueueMessage message)
         {
             SalesCatalogueServiceResponseQueueMessage fulfilmentServiceQueueMessage = JsonConvert.DeserializeObject<SalesCatalogueServiceResponseQueueMessage>(message.AsString);
+            string homeDirectoryPath = configuration["HOME"];
+            string currentUtcDateTime = DateTime.UtcNow.ToString("ddMMMyyyy");
 
-            logger.LogInformation(EventIds.CreateExchangeSetRequestStart.ToEventId(), "Create Exchange Set web job started for BatchId:{BatchId} and _X-Correlation-ID:{CorrelationId}", fulfilmentServiceQueueMessage.BatchId, fulfilmentServiceQueueMessage.CorrelationId);
+            try
+            {
+                logger.LogInformation(EventIds.CreateExchangeSetRequestStart.ToEventId(), "Create Exchange Set web job started for BatchId:{BatchId} and _X-Correlation-ID:{CorrelationId}", fulfilmentServiceQueueMessage.BatchId, fulfilmentServiceQueueMessage.CorrelationId);
 
-            await fulFilmentDataService.CreateExchangeSet(fulfilmentServiceQueueMessage);
+                await fulFilmentDataService.CreateExchangeSet(fulfilmentServiceQueueMessage);
 
-            logger.LogInformation(EventIds.CreateExchangeSetRequestCompleted.ToEventId(), "Create Exchange Set web job completed for BatchId:{BatchId} and _X-Correlation-ID:{CorrelationId}", fulfilmentServiceQueueMessage.BatchId, fulfilmentServiceQueueMessage.CorrelationId);
+                logger.LogInformation(EventIds.CreateExchangeSetRequestCompleted.ToEventId(), "Create Exchange Set web job completed for BatchId:{BatchId} and _X-Correlation-ID:{CorrelationId}", fulfilmentServiceQueueMessage.BatchId, fulfilmentServiceQueueMessage.CorrelationId);
+            }
+            catch (CustomException ex)
+            {
+                string errorFileName = "error.txt";
+                var exchangeSetBatchFolderPath = Path.Combine(homeDirectoryPath, currentUtcDateTime, fulfilmentServiceQueueMessage.BatchId);
+                fileSystemHelper.CheckAndCreateFolder(exchangeSetBatchFolderPath);
+
+                var errorFilePath = Path.Combine(exchangeSetBatchFolderPath, errorFileName);
+                await File.WriteAllTextAsync(errorFilePath, ex.ErrorMessage);
+            }
         }
     }
 }
