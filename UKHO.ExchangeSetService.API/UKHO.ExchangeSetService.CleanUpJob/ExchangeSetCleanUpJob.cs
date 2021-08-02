be7f6275bca3b02bc37e410b45f20c1c5b36f735
Ganpat Gawde <Ganpat14802@Mastek.Com>
2021-08-02 09:59:10 +0530

    7601 Add some unique identifier in logs  for web job AB#7601 AB#8255

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.CleanUpJob/ExchangeSetCleanUpJob.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.CleanUpJob/ExchangeSetCleanUpJob.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using UKHO.ExchangeSetService.CleanUpJob.Services;
+using UKHO.ExchangeSetService.Common.Configuration;
 using UKHO.ExchangeSetService.Common.Logging;
 
 namespace UKHO.ExchangeSetService.CleanUpJob
@@ -12,19 +14,22 @@ namespace UKHO.ExchangeSetService.CleanUpJob
     {
         private readonly IExchangeSetCleanUpService exchangeSetCleanUpService;
         private readonly ILogger<ExchangeSetCleanUpJob> logger;
+        private readonly IOptions<EssFulfilmentStorageConfiguration> storageConfig;
         public ExchangeSetCleanUpJob(IExchangeSetCleanUpService exchangeSetCleanUpService,
-                                     ILogger<ExchangeSetCleanUpJob> logger)
+                                     ILogger<ExchangeSetCleanUpJob> logger,
+                                     IOptions<EssFulfilmentStorageConfiguration> storageConfig)
         {
             this.exchangeSetCleanUpService = exchangeSetCleanUpService;
             this.logger = logger;
+            this.storageConfig = storageConfig;
         }
         public async Task ProcessCleanUp()
         {
-            logger.LogInformation(EventIds.ESSCleanUpJobRequestStart.ToEventId(), "Exchange set service clean up web job started at " + DateTime.Now);
+            logger.LogInformation(EventIds.ESSCleanUpJobRequestStart.ToEventId(), "Exchange set service clean up web job started at " + DateTime.Now + " for storage account name {StorageAccountName} & queue {QueueName} ", storageConfig.Value.StorageAccountName , storageConfig.Value.QueueName);
 
             await exchangeSetCleanUpService.DeleteHistoricFoldersAndFiles();
 
-            logger.LogInformation(EventIds.ESSCleanUpJobRequestCompleted.ToEventId(), "Exchange set service clean up web job completed at " + DateTime.Now);
+            logger.LogInformation(EventIds.ESSCleanUpJobRequestCompleted.ToEventId(), "Exchange set service clean up web job completed at " + DateTime.Now + " for storage account name {StorageAccountName} & queue {QueueName} ", storageConfig.Value.StorageAccountName, storageConfig.Value.QueueName);
         }
     }
 }
