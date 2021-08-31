4a846dafb04cbeb34c29f86b10b74d7eb0430eb9
Shailendra Shinde <shailen14599@mastek.com>
2021-08-31 17:51:41 +0530

    9534 - Functional tests ignored till cancellation issue resolves AB#9534

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/FunctionalTests/CreateFssBatchAndFile.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/FunctionalTests/CreateFssBatchAndFile.cs
@@ -30,6 +30,7 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
         }
 
         [Test]
+        [Ignore("Ignore this test for time being, since cancellation product data not available in file share service")]
         public async Task WhenICallTheApiWithAValidRFC1123DateTime_ThenACorrectResponseIsReturned()
         {
             string sinceDatetime = DateTime.Now.AddDays(-5).ToString("ddd, dd MMM yyyy HH':'mm':'ss 'GMT'", CultureInfo.InvariantCulture);
