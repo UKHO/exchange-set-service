8703b47d48d621138af57141a452648b9186c75c
Siddartha Mote <siddart14393@mastek.com>
2021-09-01 12:04:13 +0530

    9668 AuthScsTokenProvider and AuthFssTokenProvider excluded from code coverage AB#9668 AB#9671

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Helpers/AuthScsTokenProvider.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Helpers/AuthScsTokenProvider.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.Diagnostics.CodeAnalysis;
 using UKHO.ExchangeSetService.Common.Configuration;
 
 namespace UKHO.ExchangeSetService.Common.Helpers
 {
+    [ExcludeFromCodeCoverage] ////Excluded from code coverage as it has ADD interaction
     public class AuthScsTokenProvider : AuthTokenProvider, IAuthScsTokenProvider
     {
         public AuthScsTokenProvider(IOptions<EssManagedIdentityConfiguration> essManagedIdentityConfiguration, IDistributedCache _cache, ILogger<AuthScsTokenProvider> logger) : 
