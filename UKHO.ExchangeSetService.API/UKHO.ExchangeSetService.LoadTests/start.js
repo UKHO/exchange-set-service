e774ca8480190029f9b0948458c08f470af84281
Rashi Mishra <Rashi14748@Mastek.Com>
2021-09-08 10:43:02 +0530

    9542- updated script for different size of exchange sets AB#9542 AB# 9568

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.LoadTests/start.js
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.LoadTests/start.js
@@ -8,45 +8,70 @@ import { Trend } from 'k6/metrics';
 const runTestProductIdentifier = require('./LoadTestForProductIdentifier.js');
 const config = JSON.parse(open('./config.json'));
 const dataHelper = require('./dataHelper.js');
-const productIdentifierData_Small = dataHelper.GetProductIdentifierDataforSmallExchangeSet();
-const productIdentifierData_Medium = dataHelper.GetProductIdentifierDataforMediumExchangeSet();
+const productIdentifierData_Small_25MB = dataHelper.GetProductIdentifierDataforSmallExchangeSet_25MB();
+const productIdentifierData_Small_50MB = dataHelper.GetProductIdentifierDataforSmallExchangeSet_50MB();
+const productIdentifierData_Medium_150MB = dataHelper.GetProductIdentifierDataforMediumExchangeSet_150MB();
+const productIdentifierData_Medium_300MB = dataHelper.GetProductIdentifierDataforMediumExchangeSet_300MB();
 const productIdentifierData_Large = dataHelper.GetProductIdentifierDataforLargeExchangeSet();
-let SmallExchangeSetCreationTrend = new Trend('SmallEssCreationtime');
-let MediumExchangeSetCreationTrend = new Trend('MediumEssCreationtime');
+
+let SmallExchangeSetCreationTrendfor25MB = new Trend('SmallEssCreationtimefor25MB');
+let SmallExchangeSetCreationTrendfor50MB = new Trend('SmallEssCreationtimefor50MB');
+let MediumExchangeSetCreationTrendfor150MB = new Trend('MediumEssCreationtimefor150MB');
+let MediumExchangeSetCreationTrendfor300MB = new Trend('MediumEssCreationtimefor300MB');
 let LargeExchangeSetCreationTrend = new Trend('LargeEssCreationtime');
 let clientAuthResp = {};
 
 export let options = {
     scenarios: {
-        ESSCreationSmallExchangeSet: {
-            exec: 'ESSCreationSmallExchangeSet',
-            executor: 'per-vu-iterations',
-            startTime: '10s',
-            gracefulStop: '5s',
-            vus: 25,
-            iterations: 161,
-            maxDuration: '1h'
+        ESSCreationSmallExchangeSet_25MB: {
+            exec: 'ESSCreationSmallExchangeSet_25MB',
+            executor: 'constant-arrival-rate',
+            rate: 133,
+            timeUnit: '1m',
+            duration: '1h',
+            preAllocatedVUs: 245,
+            maxVUs: 270,
         },
-        ESSCreationMediumExchangeSet: {
-            exec: 'ESSCreationMediumExchangeSet',
-            executor: 'per-vu-iterations',
-            startTime: '10s',
-            gracefulStop: '5s',
-            vus: 5,
-            iterations: 161,
-            maxDuration: '1h'
+        ESSCreationSmallExchangeSet_50MB: {
+            exec: 'ESSCreationSmallExchangeSet_50MB',
+            executor: 'constant-arrival-rate',
+            rate: 26,
+            timeUnit: '1m',
+            duration: '1h',
+            preAllocatedVUs: 48,
+            maxVUs: 50,
+        },
+        ESSCreationMediumExchangeSet_150MB: {
+            exec: 'ESSCreationMediumExchangeSet_150MB',
+            executor: 'constant-arrival-rate',
+            rate: 27,
+            timeUnit: '5m',
+            duration: '1h',
+            preAllocatedVUs: 22,
+            maxVUs: 25,
+        },
+        ESSCreationMediumExchangeSet_300MB: {
+            exec: 'ESSCreationMediumExchangeSet_300MB',
+            executor: 'constant-arrival-rate',
+            rate: 8,
+            timeUnit: '10m',
+            duration: '1h',
+            preAllocatedVUs: 3,
+            maxVUs: 4,
         },
         ESSCreationLargeExchangeSet: {
             exec: 'ESSCreationLargeExchangeSet',
-            executor: 'per-vu-iterations',
-            startTime: '10s',
-            gracefulStop: '5s',
-            vus: 1,
-            iterations: 170,
-            maxDuration: '1h'
+            executor: 'constant-arrival-rate',
+            rate: 8,
+            timeUnit: '10m',
+            duration: '1h',
+            preAllocatedVUs: 3,
+            maxVUs: 4,
         },
     },
 };
+
+
 export function setup() {
     // client credentials authentication flow
      let essAuthResp = authenticateUsingAzure(
@@ -62,19 +87,35 @@ export function setup() {
     return clientAuthResp;
 }
 
-export function ESSCreationSmallExchangeSet(clientAuthResp) {
-    var group_duration = apiClient.GetGroupDuration('SmallEssCreation', () => {
-        runTestProductIdentifier.ESSCreation(clientAuthResp, productIdentifierData_Small, "Small");
+export function ESSCreationSmallExchangeSet_25MB(clientAuthResp) {
+    var group_duration = apiClient.GetGroupDuration('Small_25MB_EssCreation', () => {
+        runTestProductIdentifier.ESSCreation(clientAuthResp, productIdentifierData_Small_25MB, "Small_25MB");
     });
-    SmallExchangeSetCreationTrend.add(group_duration);
+    SmallExchangeSetCreationTrendfor25MB.add(group_duration);
     sleep(1);
 }
 
-export function ESSCreationMediumExchangeSet(clientAuthResp) {
-    var group_duration = apiClient.GetGroupDuration('MediumEssCreation', () => {
-        runTestProductIdentifier.ESSCreation(clientAuthResp, productIdentifierData_Medium, "Medium");
+export function ESSCreationSmallExchangeSet_50MB(clientAuthResp) {
+    var group_duration = apiClient.GetGroupDuration('Small_50MB_EssCreation', () => {
+        runTestProductIdentifier.ESSCreation(clientAuthResp, productIdentifierData_Small_50MB, "Small_50MB");
     });
-    MediumExchangeSetCreationTrend.add(group_duration);
+    SmallExchangeSetCreationTrendfor50MB.add(group_duration);
+    sleep(1);
+}
+
+export function ESSCreationMediumExchangeSet_150MB(clientAuthResp) {
+    var group_duration = apiClient.GetGroupDuration('Medium_150MB_EssCreation', () => {
+        runTestProductIdentifier.ESSCreation(clientAuthResp, productIdentifierData_Medium_150MB, "Medium_150MB");
+    });
+    MediumExchangeSetCreationTrendfor150MB.add(group_duration);
+    sleep(1);
+}
+
+export function ESSCreationMediumExchangeSet_300MB(clientAuthResp) {
+    var group_duration = apiClient.GetGroupDuration('Medium_300MB_EssCreation', () => {
+        runTestProductIdentifier.ESSCreation(clientAuthResp, productIdentifierData_Medium_300MB, "Medium_300MB");
+    });
+    MediumExchangeSetCreationTrendfor300MB.add(group_duration);
     sleep(1);
 }
 
@@ -85,6 +126,7 @@ export function ESSCreationLargeExchangeSet(clientAuthResp) {
     LargeExchangeSetCreationTrend.add(group_duration);
     sleep(1);
 }
+
 
 export function handleSummary(data) {
     return {
