85722bc711ed0dcb5670f8e73c48825a433018b8
Shailendra Shinde <shailen14599@mastek.com>
2021-09-08 15:40:33 +0530

    9834 - Removed unused reference AB#9834 AB#9863

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.LoadTests/sinceDateTimeStart.js
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.LoadTests/sinceDateTimeStart.js
@@ -1,5 +1,3 @@
-import http from "k6/http";
-import { Trend } from 'k6/metrics';
 import { sleep, group } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
