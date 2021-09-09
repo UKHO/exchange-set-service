bcc5f40eea72a332c2ffbae230f200888e8d3786
Shirin14926 <Shirin.talawdekar@mastek.com>
2021-09-09 19:10:27 +0530

    9668- add test for code coverage-improvement AB#9668 AB#9671

---
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using UKHO.ExchangeSetService.API.Extensions;
+
+namespace UKHO.ExchangeSetService.API.UnitTests.Extensions
+{
+    [TestFixture]
+    public class DateTimeExtensionsTest
+    {
+        [Test]
+        public void CheckIsValidDate()
+        {
+            bool isValidDate = DateTimeExtensions.IsValidDate("08Sep2021", out DateTime fakeFolderDateTime);
+            Assert.False(isValidDate);
+        }
+    }
+}
