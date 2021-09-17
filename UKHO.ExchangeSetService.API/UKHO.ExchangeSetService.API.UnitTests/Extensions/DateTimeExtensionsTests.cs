dcbad1328db531866cd74976bd7d69d0b5708433
Siddartha Mote <siddart14393@mastek.com>
2021-09-17 14:19:06 +0530

    9668 added other Unit Test for 'IsValidDate' as per review comment AB#9668 AB#10603

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Extensions/DateTimeExtensionsTests.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Extensions/DateTimeExtensionsTests.cs
@@ -8,9 +8,23 @@ namespace UKHO.ExchangeSetService.API.UnitTests.Extensions
     public class DateTimeExtensionsTest
     {
         [Test]
-        public void CheckIsValidDate()
+        public void CheckIsValidDateWithValidDate_ThenReturnTrue()
         {
             bool isValidDate = DateTimeExtensions.IsValidDate("08Sep2021", out DateTime fakeFolderDateTime);
+            Assert.IsTrue(isValidDate);
+        }
+
+        [Test]
+        public void CheckIsValidDateWithInvalidLeapYear_ThenReturnFalse()
+        {
+            bool isValidDate = DateTimeExtensions.IsValidDate("29Feb2001", out DateTime fakeFolderDateTime);
+            Assert.IsFalse(isValidDate);
+        }
+
+        [Test]
+        public void CheckIsValidDateWithValidLeapYear_ThenReturnTrue()
+        {
+            bool isValidDate = DateTimeExtensions.IsValidDate("29Feb2000", out DateTime fakeFolderDateTime);
             Assert.IsTrue(isValidDate);
         }
     }
