74c8e80457e62ab4284608623286ea3f26b117c8
Rashi Mishra <Rashi14748@Mastek.Com>
2021-10-04 16:08:19 +0530

    10853- update load profile for product identifier and added folders for script and helper classes AB#10853

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.LoadTests/scripts/LoadTestForProductIdentifier.js
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.LoadTests/scripts/LoadTestForProductIdentifier.js
@@ -1,14 +1,14 @@
 import http from 'k6/http';
 import { check, group, sleep } from 'k6';
 
-const apiClient = require('./clientHelper.js');
+const apiClient = require('./helper/clientHelper.js');
 
 export function ESSCreation(clientAuthResp, productIdentifierData, exchangeSetType) {
     let fssCommitStatus;
     group('ESS Creation', () => {
         let batchStatusUrl = apiClient.GetESSApiResponse("productIdentifiers", productIdentifierData, `${clientAuthResp.essToken}`, exchangeSetType);
         sleep(1);
-        console.log("batchStatusUrl", batchStatusUrl);
+       
         fssCommitStatus = apiClient.GetFSSApiResponse(JSON.parse(batchStatusUrl), `${clientAuthResp.fssToken}`);
 
         while (fssCommitStatus !== "Committed") {
@@ -20,7 +20,10 @@ export function ESSCreation(clientAuthResp, productIdentifierData, exchangeSetTy
             }
         };
     });
+    
     check(fssCommitStatus, {
         "status is Committed": fssCommitStatus === "Committed",
     })
+    console.log("batchStatusUrl", batchStatusUrl);
+    console.log("fssCommitStatus", fssCommitStatus, exchangeSetType);
 }
