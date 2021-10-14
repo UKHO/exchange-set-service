4ff80234fdd97c818f5fcb5fea03e50075c6b973
Ganpat Gawde <Ganpat14802@Mastek.Com>
2021-10-14 15:38:44 +0530

    11237 Added changes capturing response check count for product identifier endpoint AB#11237 AB#11238

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.LoadTests/scripts/LoadTestForProductIdentifier.js
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.LoadTests/scripts/LoadTestForProductIdentifier.js
@@ -4,15 +4,16 @@ import { check, group, sleep } from 'k6';
 const apiClient = require('../helper/clientHelper.js');
 
 export function ESSCreation(clientAuthResp, productIdentifierData, exchangeSetType) {
-    let fssCommitStatus;
+    let fssCommitStatus, batchDetailsUri, batchStatusUrl;
+
     group('ESS Creation', () => {
         let essResponse = apiClient.GetESSApiResponse("productIdentifiers", productIdentifierData, `${clientAuthResp.essToken}`, exchangeSetType);
         sleep(1);
-       
-        let jsonResponse = JSON.parse(essResponse.body);
-        let batchStatusUrl = JSON.stringify(jsonResponse['_links']['exchangeSetBatchStatusUri']['href']);
-        let batchDetailsUri = JSON.stringify(jsonResponse['_links']['exchangeSetBatchDetailsUri']['href']);
 
+        let jsonResponse = JSON.parse(essResponse.body);
+        batchStatusUrl = JSON.stringify(jsonResponse['_links']['exchangeSetBatchStatusUri']['href']);
+        batchDetailsUri = JSON.stringify(jsonResponse['_links']['exchangeSetBatchDetailsUri']['href']);
+    
         fssCommitStatus = apiClient.GetFSSApiResponse(JSON.parse(batchStatusUrl), `${clientAuthResp.fssToken}`);
 
         while (fssCommitStatus !== "Committed") {
@@ -23,21 +24,19 @@ export function ESSCreation(clientAuthResp, productIdentifierData, exchangeSetTy
                 break;
             }
         };
-
-        check(fssCommitStatus, {
-            "status is Committed": fssCommitStatus === "Committed",
-        })
-
-        let batchId = GetBatchId(batchDetailsUri);
-
-        console.log("batchId : " + batchId + " fssCommitStatus : " , fssCommitStatus, exchangeSetType);
-
     });
+
+    check(fssCommitStatus, {
+        "status is Committed": fssCommitStatus === "Committed",
+    })
+
+    let batchId = GetBatchId(batchDetailsUri);
+    console.log("batchId : " + batchId + " fssCommitStatus : ", fssCommitStatus, exchangeSetType);
 }
 
 export function GetBatchId(str) {
-    let revBatchDetailsUri =str.split('').reverse().join('');
-    let revBatchId =revBatchDetailsUri.substring(1, revBatchDetailsUri.indexOf("/"))
+    let revBatchDetailsUri = str.split('').reverse().join('');
+    let revBatchId = revBatchDetailsUri.substring(1, revBatchDetailsUri.indexOf("/"))
     let batchId = revBatchId.split('').reverse().join('');
     return batchId;
- }
+}
