ba4ce001f160c089aa392c1186683d0c138c54b8
Ganpat Gawde <Ganpat14802@Mastek.Com>
2021-10-20 16:30:45 +0530

    11171 Minor changes for scs essdata endpoint AB#11171 AB#11180

--- UKHO.SalesCatalogueFileShareServicesMock.API/UKHO.SalesCatalogueFileShareServicesMock.API/Models/Response/SalesCatalogueDataProductResponse.cs
+++ UKHO.SalesCatalogueFileShareServicesMock.API/UKHO.SalesCatalogueFileShareServicesMock.API/Models/Response/SalesCatalogueDataProductResponse.cs
@@ -6,25 +6,26 @@ namespace UKHO.SalesCatalogueFileShareServicesMock.API.Models.Response
 {
     public class SalesCatalogueDataProductResponse
     {
-        public string ProductName { get; set; }
         public short BaseCellEditionNumber { get; set; }
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public DateTime BaseCellIssueDate { get; set; }
         public string BaseCellLocation { get; set; }
         public int? BaseCellUpdateNumber { get; set; }
         public List<string> CancelledCellReplacements { get; set; }
+        public int? cancelledEditionNumber { get; set; }
         public decimal CellLimitEasternmostLatitude { get; set; }
         public decimal CellLimitNorthernmostLatitude { get; set; }
         public decimal CellLimitSouthernmostLatitude { get; set; }
         public decimal CellLimitWesternmostLatitude { get; set; }
         public bool Compression { get; set; }
+        public List<string> dataCoverageCoordinates { get; set; }
         public bool Encryption { get; set; }
         public int? FileSize { get; set; }
+        public string id { get; set; }
         public DateTime? IssueDateLatestUpdate { get; set; }
         public DateTime? IssueDatePreviousUpdate { get; set; }
+        public int? lastUpdateNumberPreviousEdition { get; set; }
         public int? LatestUpdateNumber { get; set; }
-        public int? LastUpdateNumberForPreviousEdition { get; set; }
-        public string TenDataCoverageCoordinates { get; set; }
-        public string ProductNameLessExtension { get; }
+        public string ProductName { get; set; }
     }
 }
