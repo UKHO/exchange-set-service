3ed8e6e36be9dbe907e7b9520c0470f5d4e82bf8
Ganpat Gawde <Ganpat14802@Mastek.Com>
2021-10-21 15:26:22 +0530

    11171- Review comments fixed AB#11171 AB#11519

--- UKHO.SalesCatalogueFileShareServicesMock.API/UKHO.SalesCatalogueFileShareServicesMock.API/Models/Response/SalesCatalogueDataProductResponse.cs
+++ UKHO.SalesCatalogueFileShareServicesMock.API/UKHO.SalesCatalogueFileShareServicesMock.API/Models/Response/SalesCatalogueDataProductResponse.cs
@@ -12,19 +12,19 @@ namespace UKHO.SalesCatalogueFileShareServicesMock.API.Models.Response
         public string BaseCellLocation { get; set; }
         public int? BaseCellUpdateNumber { get; set; }
         public List<string> CancelledCellReplacements { get; set; }
-        public int? cancelledEditionNumber { get; set; }
+        public int? CancelledEditionNumber { get; set; }
         public decimal CellLimitEasternmostLatitude { get; set; }
         public decimal CellLimitNorthernmostLatitude { get; set; }
         public decimal CellLimitSouthernmostLatitude { get; set; }
         public decimal CellLimitWesternmostLatitude { get; set; }
         public bool Compression { get; set; }
-        public List<string> dataCoverageCoordinates { get; set; }
+        public List<string> DataCoverageCoordinates { get; set; }
         public bool Encryption { get; set; }
         public int? FileSize { get; set; }
-        public string id { get; set; }
+        public string Id { get; set; }
         public DateTime? IssueDateLatestUpdate { get; set; }
         public DateTime? IssueDatePreviousUpdate { get; set; }
-        public int? lastUpdateNumberPreviousEdition { get; set; }
+        public int? LastUpdateNumberPreviousEdition { get; set; }
         public int? LatestUpdateNumber { get; set; }
         public string ProductName { get; set; }
     }
