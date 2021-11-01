348e4e3eb824b90ca067566ba2d28426828307db
Ganpat Gawde <Ganpat14802@Mastek.Com>
2021-11-01 13:15:01 +0530

    13174 code changes  AB#13174 AB#14470

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Helpers/CommonHelper.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Helpers/CommonHelper.cs
@@ -76,6 +76,8 @@ namespace UKHO.ExchangeSetService.Common.Helpers
             return Policy
                 .HandleResult<HttpResponseMessage>(r => r.StatusCode == HttpStatusCode.ServiceUnavailable)
                 .OrResult(r => r.StatusCode == HttpStatusCode.TooManyRequests)
+                .OrResult(r => r.StatusCode == HttpStatusCode.BadRequest && requestType == "File Share")
+                .OrResult(r => r.StatusCode == HttpStatusCode.InternalServerError && requestType == "File Share")
                 .WaitAndRetryAsync(retryCount, (retryAttempt) =>
                 {
                     return TimeSpan.FromSeconds(Math.Pow(sleepDuration, (retryAttempt - 1)));
