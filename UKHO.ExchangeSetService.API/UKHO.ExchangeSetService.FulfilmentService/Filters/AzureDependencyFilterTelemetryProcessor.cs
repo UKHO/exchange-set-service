f71b0ded869a85a745fb9c74fce6d47f0e81ee81
Ganpat Gawde <Ganpat14802@Mastek.Com>
2021-11-30 18:05:12 +0530

    10944 AzureDependencyFilterTelemetryProcesso class exclude from code coverage AB#10944 AB#19398

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.FulfilmentService/Filters/AzureDependencyFilterTelemetryProcessor.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.FulfilmentService/Filters/AzureDependencyFilterTelemetryProcessor.cs
@@ -2,9 +2,11 @@
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace UKHO.ExchangeSetService.FulfilmentService.Filters
 {
+    [ExcludeFromCodeCoverage]
     public class AzureDependencyFilterTelemetryProcessor : ITelemetryProcessor
     {
         private readonly ITelemetryProcessor inner;
