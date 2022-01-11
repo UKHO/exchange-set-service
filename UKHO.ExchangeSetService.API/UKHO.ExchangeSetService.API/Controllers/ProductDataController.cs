1d8da92d65b250f6d75d6f857a259131bda8e80b
Shirin14926 <Shirin.talawdekar@mastek.com>
2022-01-11 20:06:57 +0530

    23627 commit pending code changes AB#23627 AB#23944

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/ProductDataController.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/ProductDataController.cs
@@ -282,11 +282,13 @@ namespace UKHO.ExchangeSetService.API.Controllers
             var data = eventGridEvent.Data as JObject;
             var eventGridCacheDataRequest = (eventGridEvent.Data as JObject).ToObject<EventGridCacheDataRequest>();
             logger.LogInformation(EventIds.ESSClearCacheSearchDownloadEventStart.ToEventId(), "Clear cache Event started for Data:{eventGridCaheDataRequest} and _X-Correlation-ID:{correlationId}", JsonConvert.SerializeObject(data, Formatting.Indented), GetCurrentCorrelationId());
-            if (data != null && eventGridCacheDataRequest != null)
+            ////Validation
+            var validationResult = await productDataService.ValidateEventGridCacheDataRequest(eventGridCacheDataRequest);
+
+            if (!validationResult.IsValid && validationResult.HasBadRequestErrors(out List<Error> errors))
             {
                 return BuildBadRequestErrorResponse(errors);
             }
-                
             ////Business logic
             var response = await productDataService.DeleteSearchAndDownloadCacheData(eventGridCacheDataRequest);
             if (response)
