386fe2e702795620e9aa1a8f28e4166f353b5171
Shirin14926 <85286507+Shirin14926@users.noreply.github.com>
2022-01-11 19:05:18 +0530

    23666- code change and add padding for failing FTs in QC AB#23666 AB#23694 (#123)

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/Helper/FileContentHelper.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/Helper/FileContentHelper.cs
@@ -52,7 +52,7 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.Helper
             string dateAndCdType = fileContent[3];
             string formatVersionAndExchangeSetNumber = fileContent[7];
 
-            int weekNumber = CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(DateTime.UtcNow, CalendarWeekRule.FirstFullWeek, DayOfWeek.Thursday);
+            string weekNumber = CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(DateTime.UtcNow, CalendarWeekRule.FirstFullWeek, DayOfWeek.Thursday).ToString().PadLeft(2, '0');
             string year = DateTime.UtcNow.Year.ToString().Substring(DateTime.UtcNow.Year.ToString().Length - 2);
             string currentDate = DateTime.UtcNow.ToString("yyyyMMdd");
 
