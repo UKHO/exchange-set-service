938a386be9ca3eab1f272a54db1d3edc66e37f8e
Barrie Cooper <barrie.cooper@ukho.gov.uk>
2022-05-10 17:16:15 +0100

    Introduce daily_data_cap_in_gb = 5

--- Deployment/src/Modules/AppInsights/main.tf
+++ Deployment/src/Modules/AppInsights/main.tf
@@ -1,7 +1,8 @@
 resource "azurerm_application_insights" "app_insights" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  application_type    = "web"
-  tags                = var.tags
+  name                 = var.name
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  application_type     = "web"
+  daily_data_cap_in_gb = 5
+  tags                 = var.tags
 }
