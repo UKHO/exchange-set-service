006eb90e66e9d25e0f0744d7485a5a3ff6254689
Harshda Joshi <harshda15125@mastek.com>
2022-08-04 18:06:05 +0530

    53899-Added validation for basefolder location AB#53909

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.FulfilmentService/Validation/ProductDataValidator.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.FulfilmentService/Validation/ProductDataValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UKHO.ExchangeSetService.Common.Models.SalesCatalogue;
@@ -13,6 +14,7 @@ namespace UKHO.ExchangeSetService.FulfilmentService.Validation
     {
         public ProductDataValidator()
         {
+            Int16 maxValue = 100;
             RuleFor(p =>p)
               .Must(pi => pi != null)
               .WithMessage("products cannot be null or empty.");
@@ -31,8 +33,14 @@ namespace UKHO.ExchangeSetService.FulfilmentService.Validation
                       .NotEqual("M0")
                       .When(p => string.IsNullOrWhiteSpace(p.Location))
                       .WithMessage("Location cannot be null or empty");
+
+                    orders.RuleFor(x => x.Location)
+                    .Must(p => Convert.ToInt16(p.Replace("M1;B", "")) > 0 && Convert.ToInt16(p.Replace("M1;B", "")) < maxValue)
+                    .Must(p => Convert.ToInt16(p.Replace("M2;B", "")) > 0 && Convert.ToInt16(p.Replace("M2;B", "")) < maxValue)
+                    .WithMessage("Base must be in betwenn 1 - 99");
                 });
             });
+            
         }
         Task<ValidationResult> IProductDataValidator.Validate(List<Products> salesCatalogueProductResponse)
         {
