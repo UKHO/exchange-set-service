eda18dcbb2b2cb46a710ccf0e0a23537e00f90ec
JiviteshT <112479261+JiviteshT@users.noreply.github.com>
2023-03-03 08:29:27 +0000

    ABZU-69885-Add lease to pipeline (#191)

---
+++ Deployment/add-lease.ps1
@@ -0,0 +1,28 @@
+Param(
+    [Parameter(mandatory=$true)][int]$daysValid,
+	[Parameter(mandatory=$true)][string]$accessToken,
+    [Parameter(mandatory=$true)][int]$definitionId,
+    [Parameter(mandatory=$true)][string]$ownerId,
+    [Parameter(mandatory=$true)][int]$buildId,
+    [Parameter(mandatory=$true)][string]$collectionUri,
+    [Parameter(mandatory=$true)][string]$teamProject    
+)
+
+try{
+    $contentType = "application/json";
+    $headers = @{ Authorization = 'Bearer $accessToken' };
+    $rawRequest = @{ daysValid = $daysValid; definitionId = $definitionId; ownerId = $ownerId; protectPipeline = $false; runId = $buildId };
+    $request = ConvertTo-Json @($rawRequest);
+    $uri = "$collectionUri$teamProject/_apis/build/retention/leases?api-version=7.0";
+
+    Write-Host $request
+    Write-Host $uri
+
+    Invoke-RestMethod -uri $uri -method POST -Headers $headers -ContentType $contentType -Body $request;
+
+    Write-Host "Pipeline will be retained for $daysValid days"
+}
+catch{
+   Write-Host $_
+   Write-Host "##vso[task.complete result=SucceededWithIssues;]"
+}
