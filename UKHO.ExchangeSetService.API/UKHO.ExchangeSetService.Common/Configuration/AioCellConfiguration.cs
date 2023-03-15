68f5c934367d400a48e506dd2d50b7d74d97193b
Shailendra Shinde <shailen14599@mastek.com>
2023-03-15 09:57:56 +0530

    71795 - Added AIO cell list configuration AB#71795 AB#71903

---
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Configuration/AioCellConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace UKHO.ExchangeSetService.Common.Configuration
+{
+    public static class AioCellConfiguration
+    {
+        public static List<string> AioCells { get; set; } = new List<string>() { "GB800001" };
+    }
+}
