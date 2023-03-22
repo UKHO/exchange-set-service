7640bbae92cfbb91e5dfde2f2af732652991f322
Richard Hernandez <richard.hernandez@ukho.gov.uk>
2023-03-22 18:26:12 +0000

    Clean out Old code

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.CleanUpJob/Program.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.CleanUpJob/Program.cs
@@ -29,46 +29,11 @@ namespace UKHO.ExchangeSetService.CleanUpJob
     public static class Program
     {
         private static string AssemblyVersion = Assembly.GetExecutingAssembly().GetCustomAttributes<AssemblyFileVersionAttribute>().Single().Version;
-        //static InMemoryChannel aiChannel = new InMemoryChannel();
-
        
         static async Task Main()
         {
-            //try
-            //{
-            //    var delayTime = 5000;
-
-            //    //Build configuration
-            //    var configuration = BuildConfiguration();
-
-            //    var serviceCollection = new ServiceCollection();
-
-            //    //Configure required services
-            //    ConfigureServices(serviceCollection, configuration);
-
-            //    //Create service provider. This will be used in logging.
-            //    var serviceProvider = serviceCollection.BuildServiceProvider();
-
-            //    try
-            //    {
-            //        await serviceProvider.GetService<ExchangeSetCleanUpJob>().ProcessCleanUp();
-            //    }
-            //    finally
-            //    {
-            //        //Ensure all buffered app insights logs are flushed into Azure
-            //        aiChannel.Flush();
-            //        await Task.Delay(delayTime);
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine($"Exception: {ex.Message}{Environment.NewLine}Stack trace: {ex.StackTrace}");
-            //    throw;
-            //}
-
+            
             var builder = new HostBuilder();
-
-
 
             builder.ConfigureAppConfiguration((context, configBuilder) => 
             {
@@ -150,7 +115,7 @@ namespace UKHO.ExchangeSetService.CleanUpJob
                 services.Configure<EssFulfilmentStorageConfiguration>(context.Configuration.GetSection("EssFulfilmentStorageConfiguration"));
                 services.Configure<CleanUpConfiguration>(context.Configuration.GetSection("CleanUpConfiguration"));
 
-                //services.AddSingleton<IConfiguration>(context.Configuration);
+                services.AddSingleton<IConfiguration>(context.Configuration);
                 services.AddTransient<ExchangeSetCleanUpJob>();
                 services.AddScoped<IAzureBlobStorageClient, AzureBlobStorageClient>();
                 services.AddScoped<IExchangeSetCleanUpService, ExchangeSetCleanUpService>();
@@ -171,107 +136,6 @@ namespace UKHO.ExchangeSetService.CleanUpJob
                 await host.RunAsync();
             }
         }
-
-        //private static IConfigurationRoot BuildConfiguration()
-        //{
-        //    var configBuilder =
-        //        new ConfigurationBuilder()
-        //        .AddJsonFile("appsettings.json", true, true);
-
-        //    //Add environment specific configuration files.
-        //    var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-        //    if (!string.IsNullOrWhiteSpace(environmentName))
-        //    {
-        //        configBuilder.AddJsonFile($"appsettings.{environmentName}.json", optional: true);
-        //    }
-
-        //    var tempConfig = configBuilder.Build();
-        //    string kvServiceUri = tempConfig["KeyVaultSettings:ServiceUri"];
-        //    if (!string.IsNullOrWhiteSpace(kvServiceUri))
-        //    {
-        //        var secretClient = new SecretClient(new Uri(kvServiceUri), new DefaultAzureCredential(
-        //                                            new DefaultAzureCredentialOptions { ManagedIdentityClientId = tempConfig["ESSManagedIdentity:ClientId"] }));
-        //        configBuilder.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
-        //    }
-        //    #if DEBUG
-        //    //Add development overrides configuration
-        //    configBuilder.AddJsonFile("appsettings.local.overrides.json", true, true);
-        //    #endif
-
-        //    //Add environment variables
-        //    configBuilder.AddEnvironmentVariables();
-
-        //    return configBuilder.Build();
-        //}
-
-        //private static void ConfigureServices(IServiceCollection serviceCollection, IConfiguration configuration)
-        //{
-        //    //Add logging
-        //    serviceCollection.AddLogging(loggingBuilder =>
-        //    {
-        //        loggingBuilder.AddConfiguration(configuration.GetSection("Logging"));
-
-        //        string instrumentationKey = configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
-        //        if (!string.IsNullOrEmpty(instrumentationKey))
-        //        {
-        //            loggingBuilder.AddApplicationInsights(
-        //                configureTelemetryConfiguration: (config) => config.ConnectionString = instrumentationKey,
-        //                configureApplicationInsightsLoggerOptions: (options) => { }
-        //                );
-        //        }
-
-        //        #if DEBUG
-        //        loggingBuilder.AddSerilog(new LoggerConfiguration()
-        //                        .WriteTo.File("Logs/UKHO.ExchangeSetService.CleanUpLogs-.txt", rollingInterval: RollingInterval.Day, outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] [{SourceContext}] {Message}{NewLine}{Exception}")
-        //                        .MinimumLevel.Information()
-        //                        .MinimumLevel.Override("UKHO", LogEventLevel.Debug)
-        //                        .CreateLogger(), dispose: true);
-        //        #endif
-
-        //        loggingBuilder.AddConsole();
-        //        loggingBuilder.AddDebug();
-
-        //        EventHubLoggingConfiguration eventhubConfig = configuration.GetSection("EventHubLoggingConfiguration").Get<EventHubLoggingConfiguration>();
-
-        //        if (!string.IsNullOrWhiteSpace(eventhubConfig.ConnectionString))
-        //        {
-        //            loggingBuilder.AddEventHub(config =>
-        //            {
-        //                config.Environment = eventhubConfig.Environment;
-        //                config.DefaultMinimumLogLevel =
-        //                    (LogLevel)Enum.Parse(typeof(LogLevel), eventhubConfig.MinimumLoggingLevel, true);
-        //                config.MinimumLogLevels["UKHO"] =
-        //                    (LogLevel)Enum.Parse(typeof(LogLevel), eventhubConfig.UkhoMinimumLoggingLevel, true);
-        //                config.EventHubConnectionString = eventhubConfig.ConnectionString;
-        //                config.EventHubEntityPath = eventhubConfig.EntityPath;
-        //                config.System = eventhubConfig.System;
-        //                config.Service = eventhubConfig.Service;
-        //                config.NodeName = eventhubConfig.NodeName;
-        //                config.AdditionalValuesProvider = additionalValues =>
-        //                {
-        //                    additionalValues["_AssemblyVersion"] = AssemblyVersion;
-        //                };
-        //            });
-        //        }
-        //    });
-            
-        //    serviceCollection.Configure<TelemetryConfiguration>(
-        //        (config) =>
-        //        {
-        //            config.TelemetryChannel = aiChannel;
-        //            config.ConnectionString = configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
-        //        }
-        //    );
-
-        //    serviceCollection.Configure<EssFulfilmentStorageConfiguration>(configuration.GetSection("EssFulfilmentStorageConfiguration"));
-        //    serviceCollection.Configure<CleanUpConfiguration>(configuration.GetSection("CleanUpConfiguration"));
-
-        //    serviceCollection.AddSingleton<IConfiguration>(configuration);
-        //    serviceCollection.AddTransient<ExchangeSetCleanUpJob>();
-        //    serviceCollection.AddScoped<IAzureBlobStorageClient, AzureBlobStorageClient>();
-        //    serviceCollection.AddScoped<IExchangeSetCleanUpService, ExchangeSetCleanUpService>();
-        //    serviceCollection.AddScoped<ISalesCatalogueStorageService, SalesCatalogueStorageService>();
-        //    serviceCollection.AddScoped<IAzureFileSystemHelper, AzureFileSystemHelper>();
-        //}
+        
     }
 }
