0e369df78e63252181c0c6a6c0cc9c9c01e58442
Arit Sarkar <arit14975@mastek.com>
2023-04-13 13:14:29 +0530

    71646 - Added Ft AB#71646 AB#76533

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/FunctionalTests/ExchangeSetWhenAioIsEnabled.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/FunctionalTests/ExchangeSetWhenAioIsEnabled.cs
@@ -12,7 +12,6 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
         private TestConfiguration Config { get; set; }
         private string DownloadedFolderPath { get; set; }
 
-
         [OneTimeSetUp]
         public async Task SetupAsync()
         {
@@ -23,7 +22,7 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
 
         //Product Backlog Item 71610: Create empty SERIAL.AIO file and add to AIO exchange set
         [Test]
-        [Category("SmokeTest")]
+        [Category("SmokeTest-AIOEnabled")]
         public async Task WhenIDownloadAioZipExchangeSet_ThenASerialAioFileIsAvailable()
         {
             foreach(string batchId in Config.AIOConfig.AioExchangeSetBatchIds)
@@ -38,7 +37,7 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
         }
 
         [Test]
-        [Category("SmokeTest")]
+        [Category("SmokeTest-AIOEnabled")]
         public async Task WhenIDownloadAioZipExchangeSet_ThenAReadmeTxtFileIsAvailableAsync()
         {
             foreach (string batchId in Config.AIOConfig.AioExchangeSetBatchIds)
@@ -57,7 +56,7 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
 
         //Product Backlog Item 74322: AIO exchange set ENC Data Set files & Signature Files
         [Test]
-        [Category("SmokeTest")]
+        [Category("SmokeTest-AIOEnabled")]
         public async Task WhenIDownloadAioZipExchangeSet_ThenEncFilesAreAvailable()
         {
             foreach (string batchId in Config.AIOConfig.AioExchangeSetBatchIds)
@@ -66,6 +65,37 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
 
                 int count = Directory.GetFiles(Path.Combine(DownloadedFolderPath, Config.AIOConfig.AioEncTempPath)).Length;
                 Assert.IsTrue(count > 0, $"Downloaded Enc files count is 0, Instead of expected count should be greater than 0");
+
+                FileContentHelper.DeleteDirectory(Config.AIOConfig.AioExchangeSetFileName);
+            }
+        }
+
+        //Product Backlog Item 72017: Create empty PRODUCTS.TXT file & add to AIO exchange set
+        [Test]
+        [Category("SmokeTest-AIOEnabled")]
+        public async Task WhenIDownloadAioZipExchangeSet_ThenAProductTxtFileIsAvailable()
+        {
+            foreach (string batchId in Config.AIOConfig.AioExchangeSetBatchIds)
+            {
+                DownloadedFolderPath = await FileContentHelper.DownloadAndExtractAioZip(FssJwtToken, batchId);
+
+                bool checkFile = FssBatchHelper.CheckforFileExist(Path.Combine(DownloadedFolderPath, Config.ExchangeSetProductFilePath), Config.ExchangeSetProductFile);
+                Assert.IsTrue(checkFile, $"{Config.ExchangeSetProductFile} File not Exist in the specified folder path : {DownloadedFolderPath}");
+
+                FileContentHelper.DeleteDirectory(Config.AIOConfig.AioExchangeSetFileName);
+            }
+        }
+
+        [Test]
+        [Category("SmokeTest-AIOEnabled")]
+        public async Task WhenIDownloadAioZipExchangeSet_ThenCatalog031IsAvailable()
+        {
+            foreach (string batchId in Config.AIOConfig.AioExchangeSetBatchIds)
+            {
+                DownloadedFolderPath = await FileContentHelper.DownloadAndExtractAioZip(FssJwtToken, batchId);
+
+                bool checkFile = FssBatchHelper.CheckforFileExist(Path.Combine(DownloadedFolderPath, Config.ExchangeSetEncRootFolder), Config.ExchangeSetCatalogueFile);
+                Assert.IsTrue(checkFile, $"File not Exist in the specified folder path : {Path.Combine(DownloadedFolderPath, Config.ExchangeSetCatalogueFile)}");
 
                 FileContentHelper.DeleteDirectory(Config.AIOConfig.AioExchangeSetFileName);
             }
