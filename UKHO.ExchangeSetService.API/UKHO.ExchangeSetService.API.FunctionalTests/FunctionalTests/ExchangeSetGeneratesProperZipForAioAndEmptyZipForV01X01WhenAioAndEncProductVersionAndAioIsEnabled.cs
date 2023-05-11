17742b065d30ffa8d9e985a8d570a81486fe2cba
Mayuresh Gawde <mayuresh10661@mastek.com>
2023-05-11 11:18:16 +0530

    77585 Updated FTs AB#77585 AB#79308

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/FunctionalTests/ExchangeSetGeneratesProperZipForAioAndEmptyZipForV01X01WhenAioAndEncProductVersionAndAioIsEnabled.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/FunctionalTests/ExchangeSetGeneratesProperZipForAioAndEmptyZipForV01X01WhenAioAndEncProductVersionAndAioIsEnabled.cs
@@ -10,21 +10,9 @@ using UKHO.ExchangeSetService.API.FunctionalTests.Models;
 namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
 {
     [TestFixture]
-    class ExchangeSetGeneratesProperZipForAioAndEmptyZipForV01X01WhenAioAndEncProductVersionAndAioIsEnabled
+    class ExchangeSetGeneratesProperZipForAioAndEmptyZipForV01X01WhenAioAndEncProductVersionAndAioIsEnabled : ObjectStorage
     {
-        private string EssJwtToken { get; set; }
-        private string FssJwtToken { get; set; }
-        private ExchangeSetApiClient ExchangeSetApiClient { get; set; }
-        private TestConfiguration Config { get; set; }
-        public DataHelper DataHelper { get; set; }
-        private HttpResponseMessage ApiEssResponse { get; set; }
-        private string AioDownloadedFolderPath;
-        private string EncDownloadedFolderPath;
-        private SalesCatalogueApiClient ScsApiClient { get; set; }
-        private string ScsJwtToken { get; set; }
-        private FssApiClient FssApiClient { get; set; }
-        private List<ProductVersionModel> ProductVersionData { get; set; }
-
+        
         //Product Backlog Item 77585: ESS : Empty AIO Exchange Set Creation
         [OneTimeSetUp]
         public async Task SetupAsync()
