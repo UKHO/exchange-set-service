ed6a76e0e1dc02cae8f0853c784f50d7a640d5d3
Hugo Burgess <hugo.burgess@ukho.gov.uk>
2023-07-19 16:15:33 +0100

    Abzu 67071 - add security headers

---
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Filters/PermissionsPolicyHeaderMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace UKHO.ExchangeSetService.API.Filters
+{
+    [ExcludeFromCodeCoverage] //Used in Startup.cs
+    public class PermissionsPolicyHeaderMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public PermissionsPolicyHeaderMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Response.Headers.Add("Permissions-Policy", "camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), usb=()");
+            return _next(httpContext);
+        }
+    }
+
+    public static class PermissionsPolicyHeaderMiddlewareExtensions
+    {
+        /// <summary>
+        /// Adds a middleware to the ASP.NET Core pipeline that sets the Permissions-Policy header.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UsePermissionsPolicyHeader(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<PermissionsPolicyHeaderMiddleware>();
+        }
+    }
+}
