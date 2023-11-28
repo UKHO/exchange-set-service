79f8484d35d21b56e8b7f1b4abe9aec089d7a339
Daniel Owen <adanielo@Engineering.ukho.gov.uk>
2023-11-28 09:58:14 +0000

    Fix

--- Deployment/src/main.tf
+++ Deployment/src/main.tf
@@ -126,86 +126,86 @@ module "fulfilment_storage" {
   service_name                          = local.service_name
 }
 
-module "key_vault" {
-  source              = "./Modules/KeyVault"
-  name                = local.key_vault_name
-  resource_group_name = azurerm_resource_group.rg.name
-  env_name            = local.env_name
-  tenant_id           = module.user_identity.ess_service_identity_tenant_id
-  location            = azurerm_resource_group.rg.location
-  allowed_ips         = var.allowed_ips  
-  subnet_id           = data.azurerm_subnet.main_subnet.id
-  agent_subnet        = data.azurerm_subnet.agent_subnet.id
-  read_access_objects = {
-    "ess_service_identity" = module.user_identity.ess_service_identity_principal_id   
-  }
-  secrets = merge(
-      {
-        "EventHubLoggingConfiguration--ConnectionString"            = module.eventhub.log_primary_connection_string
-        "EventHubLoggingConfiguration--EntityPath"                  = module.eventhub.entity_path
-        "ESSFulfilmentConfiguration--StorageAccountName"            = module.fulfilment_storage.small_exchange_set_name
-        "ESSFulfilmentConfiguration--StorageAccountKey"             = module.fulfilment_storage.small_exchange_set_primary_access_key
-        "ESSFulfilmentConfiguration--SmallExchangeSetAccountName"   = module.fulfilment_storage.small_exchange_set_name
-        "ESSFulfilmentConfiguration--SmallExchangeSetAccountKey"    = module.fulfilment_storage.small_exchange_set_primary_access_key
-        "ESSFulfilmentConfiguration--MediumExchangeSetAccountName"  = module.fulfilment_storage.medium_exchange_set_name
-        "ESSFulfilmentConfiguration--MediumExchangeSetAccountKey"   = module.fulfilment_storage.medium_exchange_set_primary_access_key
-        "ESSFulfilmentConfiguration--LargeExchangeSetAccountName"   = module.fulfilment_storage.large_exchange_set_name
-        "ESSFulfilmentConfiguration--LargeExchangeSetAccountKey"    = module.fulfilment_storage.large_exchange_set_primary_access_key
-        "CacheConfiguration--CacheStorageAccountName"               = module.cache_storage.cache_storage_name
-        "CacheConfiguration--CacheStorageAccountKey"                = module.cache_storage.cache_storage_primary_access_key
-      },
-      module.fulfilment_webapp.small_exchange_set_scm_credentials,
-      module.fulfilment_webapp.medium_exchange_set_scm_credentials,
-      module.fulfilment_webapp.large_exchange_set_scm_credentials
-  )
-  tags                                         = local.tags
-}
+# module "key_vault" {
+#   source              = "./Modules/KeyVault"
+#   name                = local.key_vault_name
+#   resource_group_name = azurerm_resource_group.rg.name
+#   env_name            = local.env_name
+#   tenant_id           = module.user_identity.ess_service_identity_tenant_id
+#   location            = azurerm_resource_group.rg.location
+#   allowed_ips         = var.allowed_ips  
+#   subnet_id           = data.azurerm_subnet.main_subnet.id
+#   agent_subnet        = data.azurerm_subnet.agent_subnet.id
+#   read_access_objects = {
+#     "ess_service_identity" = module.user_identity.ess_service_identity_principal_id   
+#   }
+#   secrets = merge(
+#       {
+#         "EventHubLoggingConfiguration--ConnectionString"            = module.eventhub.log_primary_connection_string
+#         "EventHubLoggingConfiguration--EntityPath"                  = module.eventhub.entity_path
+#         "ESSFulfilmentConfiguration--StorageAccountName"            = module.fulfilment_storage.small_exchange_set_name
+#         "ESSFulfilmentConfiguration--StorageAccountKey"             = module.fulfilment_storage.small_exchange_set_primary_access_key
+#         "ESSFulfilmentConfiguration--SmallExchangeSetAccountName"   = module.fulfilment_storage.small_exchange_set_name
+#         "ESSFulfilmentConfiguration--SmallExchangeSetAccountKey"    = module.fulfilment_storage.small_exchange_set_primary_access_key
+#         "ESSFulfilmentConfiguration--MediumExchangeSetAccountName"  = module.fulfilment_storage.medium_exchange_set_name
+#         "ESSFulfilmentConfiguration--MediumExchangeSetAccountKey"   = module.fulfilment_storage.medium_exchange_set_primary_access_key
+#         "ESSFulfilmentConfiguration--LargeExchangeSetAccountName"   = module.fulfilment_storage.large_exchange_set_name
+#         "ESSFulfilmentConfiguration--LargeExchangeSetAccountKey"    = module.fulfilment_storage.large_exchange_set_primary_access_key
+#         "CacheConfiguration--CacheStorageAccountName"               = module.cache_storage.cache_storage_name
+#         "CacheConfiguration--CacheStorageAccountKey"                = module.cache_storage.cache_storage_primary_access_key
+#       },
+#       module.fulfilment_webapp.small_exchange_set_scm_credentials,
+#       module.fulfilment_webapp.medium_exchange_set_scm_credentials,
+#       module.fulfilment_webapp.large_exchange_set_scm_credentials
+#   )
+#   tags                                         = local.tags
+# }
 
-module "fulfilment_keyvaults" {
-  source                                    = "./Modules/FulfilmentKeyVault"
-  service_name                              = local.service_name
-  resource_group_name                       = azurerm_resource_group.rg.name
-  env_name                                  = local.env_name
-  tenant_id                                 = module.user_identity.ess_service_identity_tenant_id
-  location                                  = azurerm_resource_group.rg.location
-  allowed_ips                               = var.allowed_ips 
-  small_exchange_set_subnets                = data.azurerm_subnet.small_exchange_set_subnet[*].id
-  medium_exchange_set_subnets               = data.azurerm_subnet.medium_exchange_set_subnet[*].id
-  large_exchange_set_subnets                = data.azurerm_subnet.large_exchange_set_subnet[*].id
-  agent_subnet                              = data.azurerm_subnet.agent_subnet.id
-    read_access_objects = {
-        "ess_service_identity" = module.user_identity.ess_service_identity_principal_id
-  }
-  small_exchange_set_secrets = {
-    "EventHubLoggingConfiguration--ConnectionString"            = module.eventhub.log_primary_connection_string
-    "EventHubLoggingConfiguration--EntityPath"                  = module.eventhub.entity_path
-    "ESSFulfilmentStorageConfiguration--StorageAccountName"     = module.fulfilment_storage.small_exchange_set_name
-    "ESSFulfilmentStorageConfiguration--StorageAccountKey"      = module.fulfilment_storage.small_exchange_set_primary_access_key
-    "AzureWebJobsStorage"                                       = module.fulfilment_storage.small_exchange_set_connection_string
-    "CacheConfiguration--CacheStorageAccountName"               = module.cache_storage.cache_storage_name
-    "CacheConfiguration--CacheStorageAccountKey"                = module.cache_storage.cache_storage_primary_access_key
+# module "fulfilment_keyvaults" {
+#   source                                    = "./Modules/FulfilmentKeyVault"
+#   service_name                              = local.service_name
+#   resource_group_name                       = azurerm_resource_group.rg.name
+#   env_name                                  = local.env_name
+#   tenant_id                                 = module.user_identity.ess_service_identity_tenant_id
+#   location                                  = azurerm_resource_group.rg.location
+#   allowed_ips                               = var.allowed_ips 
+#   small_exchange_set_subnets                = data.azurerm_subnet.small_exchange_set_subnet[*].id
+#   medium_exchange_set_subnets               = data.azurerm_subnet.medium_exchange_set_subnet[*].id
+#   large_exchange_set_subnets                = data.azurerm_subnet.large_exchange_set_subnet[*].id
+#   agent_subnet                              = data.azurerm_subnet.agent_subnet.id
+#     read_access_objects = {
+#         "ess_service_identity" = module.user_identity.ess_service_identity_principal_id
+#   }
+#   small_exchange_set_secrets = {
+#     "EventHubLoggingConfiguration--ConnectionString"            = module.eventhub.log_primary_connection_string
+#     "EventHubLoggingConfiguration--EntityPath"                  = module.eventhub.entity_path
+#     "ESSFulfilmentStorageConfiguration--StorageAccountName"     = module.fulfilment_storage.small_exchange_set_name
+#     "ESSFulfilmentStorageConfiguration--StorageAccountKey"      = module.fulfilment_storage.small_exchange_set_primary_access_key
+#     "AzureWebJobsStorage"                                       = module.fulfilment_storage.small_exchange_set_connection_string
+#     "CacheConfiguration--CacheStorageAccountName"               = module.cache_storage.cache_storage_name
+#     "CacheConfiguration--CacheStorageAccountKey"                = module.cache_storage.cache_storage_primary_access_key
 
-  }
-  medium_exchange_set_secrets = {
-    "EventHubLoggingConfiguration--ConnectionString"            = module.eventhub.log_primary_connection_string
-    "EventHubLoggingConfiguration--EntityPath"                  = module.eventhub.entity_path
-    "ESSFulfilmentStorageConfiguration--StorageAccountName"     = module.fulfilment_storage.medium_exchange_set_name
-    "ESSFulfilmentStorageConfiguration--StorageAccountKey"      = module.fulfilment_storage.medium_exchange_set_primary_access_key
-    "AzureWebJobsStorage"                                       = module.fulfilment_storage.medium_exchange_set_connection_string
-    "CacheConfiguration--CacheStorageAccountName"               = module.cache_storage.cache_storage_name
-    "CacheConfiguration--CacheStorageAccountKey"                = module.cache_storage.cache_storage_primary_access_key
-  }
-  large_exchange_set_secrets = {
-    "EventHubLoggingConfiguration--ConnectionString"            = module.eventhub.log_primary_connection_string
-    "EventHubLoggingConfiguration--EntityPath"                  = module.eventhub.entity_path
-    "ESSFulfilmentStorageConfiguration--StorageAccountName"     = module.fulfilment_storage.large_exchange_set_name
-    "ESSFulfilmentStorageConfiguration--StorageAccountKey"      = module.fulfilment_storage.large_exchange_set_primary_access_key
-    "AzureWebJobsStorage"                                       = module.fulfilment_storage.large_exchange_set_connection_string
-    "CacheConfiguration--CacheStorageAccountName"               = module.cache_storage.cache_storage_name
-    "CacheConfiguration--CacheStorageAccountKey"                = module.cache_storage.cache_storage_primary_access_key
-  }
-  tags                                      = local.tags
-}
+#   }
+#   medium_exchange_set_secrets = {
+#     "EventHubLoggingConfiguration--ConnectionString"            = module.eventhub.log_primary_connection_string
+#     "EventHubLoggingConfiguration--EntityPath"                  = module.eventhub.entity_path
+#     "ESSFulfilmentStorageConfiguration--StorageAccountName"     = module.fulfilment_storage.medium_exchange_set_name
+#     "ESSFulfilmentStorageConfiguration--StorageAccountKey"      = module.fulfilment_storage.medium_exchange_set_primary_access_key
+#     "AzureWebJobsStorage"                                       = module.fulfilment_storage.medium_exchange_set_connection_string
+#     "CacheConfiguration--CacheStorageAccountName"               = module.cache_storage.cache_storage_name
+#     "CacheConfiguration--CacheStorageAccountKey"                = module.cache_storage.cache_storage_primary_access_key
+#   }
+#   large_exchange_set_secrets = {
+#     "EventHubLoggingConfiguration--ConnectionString"            = module.eventhub.log_primary_connection_string
+#     "EventHubLoggingConfiguration--EntityPath"                  = module.eventhub.entity_path
+#     "ESSFulfilmentStorageConfiguration--StorageAccountName"     = module.fulfilment_storage.large_exchange_set_name
+#     "ESSFulfilmentStorageConfiguration--StorageAccountKey"      = module.fulfilment_storage.large_exchange_set_primary_access_key
+#     "AzureWebJobsStorage"                                       = module.fulfilment_storage.large_exchange_set_connection_string
+#     "CacheConfiguration--CacheStorageAccountName"               = module.cache_storage.cache_storage_name
+#     "CacheConfiguration--CacheStorageAccountKey"                = module.cache_storage.cache_storage_primary_access_key
+#   }
+#   tags                                      = local.tags
+# }
 
 module "azure-dashboard" {
   source         = "./Modules/azuredashboard"
