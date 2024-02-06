723847dd80da4b8772e051f61ff3e47b5b247888
Abhijeet Naik <abhijee14992@mastek.com>
2024-02-06 19:02:28 +0530

    140109 -  Added unit test cases for BespokeFilterAttribute AB#140109 AB#140999

---
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Filters/BespokeFilterAttributeTests.cs
@@ -0,0 +1,78 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using UKHO.ExchangeSetService.API.Filters;
+
+namespace UKHO.ExchangeSetService.API.UnitTests.Filters
+{
+    public class BespokeFilterAttributeTests
+    {
+        private IConfiguration fakeConfiguration;
+        private BespokeFilterAttribute bespokeFilterAttribute;
+        private ActionExecutingContext actionExecutingContext;
+        private ActionExecutedContext actionExecutedContext;
+        public const string TokenAudience = "aud";
+        public const string IsUnencrypted = "IsUnencrypted";
+        public const string ESSAzureADConfigurationClientId = "ESSAzureADConfiguration:ClientId";
+        readonly HttpContext httpContext = new DefaultHttpContext();
+
+        [SetUp]
+        public void Setup()
+        {
+            fakeConfiguration = A.Fake<IConfiguration>();
+            bespokeFilterAttribute = new BespokeFilterAttribute(fakeConfiguration);
+
+            var claims = new List<Claim>()
+            {
+                    new Claim(TokenAudience,"80a6c68b-59aa-49a4-939a-7968ff79d676"),
+            };
+            var identity = httpContext.User.Identities.FirstOrDefault();
+            identity.AddClaims(claims);
+        }
+
+        [Test]
+        public void WhenIsUnencyptedParameterIsFalseAndAzureADClientIDIsNotEqualsWithTokenAudience_ThenReturnNextRequest()
+        {
+            var dictionary = new Dictionary<string, StringValues>
+            {
+                { IsUnencrypted, "false" }
+            };
+            httpContext.Request.Query = new QueryCollection(dictionary);
+
+            fakeConfiguration[ESSAzureADConfigurationClientId] = "80a6c68b-59aa-49a4-939a-7968ff79d678";
+
+            var actionContext = new ActionContext(httpContext, new Microsoft.AspNetCore.Routing.RouteData(), new());
+            actionExecutingContext = new ActionExecutingContext(actionContext, new List<IFilterMetadata>(), new Dictionary<string, object>(), bespokeFilterAttribute);
+            actionExecutedContext = new ActionExecutedContext(actionContext, new List<IFilterMetadata>(), null);
+            var result =  bespokeFilterAttribute.OnActionExecutionAsync(actionExecutingContext, () => Task.FromResult(actionExecutedContext));
+            result.IsCompletedSuccessfully.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task WhenIsUnencyptedParameterIsTrueAndAzureADClientIDIsNotEqualsWithTokenAudience_ThenReturnUnauthorized()
+        {
+            var dictionary = new Dictionary<string, StringValues>
+            {
+                { IsUnencrypted, "true" }
+            };
+
+            httpContext.Request.Query = new QueryCollection(dictionary);
+            fakeConfiguration[ESSAzureADConfigurationClientId] = "80a6c68b-59ab-49a4-939a-7968ff79d678";
+
+            var actionContext = new ActionContext(httpContext, new Microsoft.AspNetCore.Routing.RouteData(), new());
+            actionExecutingContext = new ActionExecutingContext(actionContext, new List<IFilterMetadata>(), new Dictionary<string, object>(), bespokeFilterAttribute);
+            actionExecutedContext = new ActionExecutedContext(actionContext, new List<IFilterMetadata>(), null);
+            await bespokeFilterAttribute.OnActionExecutionAsync(actionExecutingContext, () => Task.FromResult(actionExecutedContext));
+            httpContext.Response.StatusCode.Should().Be(StatusCodes.Status401Unauthorized);
+        }
+    }
+}
--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Filters/BespokeFilterAttribute.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Filters/BespokeFilterAttribute.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Configuration;
-using System.Diagnostics.CodeAnalysis;
+using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -10,25 +10,30 @@ namespace UKHO.ExchangeSetService.API.Filters
     /// <summary>
     /// 140109 : ESS API :- Add authorization to allow only UKHO people to create unencrypted exchange set (Bespoke Exchange Set)
     /// </summary>
-    [ExcludeFromCodeCoverage]
     public class BespokeFilterAttribute : ActionFilterAttribute
     {
         private readonly IConfiguration configuration;
+        private const string TokenAudience = "aud";
+        private const string IsUnencrypted = "IsUnencrypted";
+        private const string ESSAzureADConfigurationClientId = "ESSAzureADConfiguration:ClientId";
         public BespokeFilterAttribute(IConfiguration configuration)
         {
             this.configuration = configuration;
         }
         public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
-            string tokenAudience = context.HttpContext.User.FindFirstValue("aud");
-            string isUnencrypted = context.HttpContext.Request.Query["IsUnencrypted"];
-            string azureADClientID = configuration["ESSAzureADConfiguration:ClientId"];
+            string tokenAudience = context.HttpContext.User.FindFirstValue(TokenAudience);
+            bool isUnencrypted = Convert.ToBoolean(context.HttpContext.Request.Query[IsUnencrypted]);
+            string azureADClientID = configuration[ESSAzureADConfigurationClientId];
 
             //If request is Bespoke exchange set and user is Non UKHO
-            if (isUnencrypted == "true" && azureADClientID != tokenAudience)
+            if (isUnencrypted)
             {
-                context.HttpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                return;
+                if (azureADClientID != tokenAudience)
+                {
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    return;
+                }
             }
             await next();
         }
