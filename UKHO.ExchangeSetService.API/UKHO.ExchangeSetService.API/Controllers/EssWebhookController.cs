109499aad105e1a8e6349370d85bd66238b44422
Richard Hernandez <richard.hernandez@ukho.gov.uk>
2024-02-07 14:06:01 +0000

    Interim changes

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/EssWebhookController.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/EssWebhookController.cs
@@ -63,11 +63,11 @@ namespace UKHO.ExchangeSetService.API.Controllers
                 Logger.LogInformation(EventIds.ESSClearCacheSearchDownloadEventCompleted.ToEventId(), "Clear Cache Event completed as Azure AD Authentication failed with OK response and _X-Correlation-ID:{correlationId}", GetCurrentCorrelationId());
                 return GetCacheResponse();
             }
-            
-            var eventGridEvent = new CustomEventGridEvent();
-            JsonConvert.PopulateObject(request.ToString(), eventGridEvent);          
-            var data = (eventGridEvent.Data as JObject).ToObject<EnterpriseEventCacheDataRequest>();
 
+            //var eventGridEvent = new CustomEventGridEvent();
+            //JsonConvert.PopulateObject(request.ToString(), eventGridEvent);
+            //var data = (eventGridEvent.Data as JObject).ToObject<EnterpriseEventCacheDataRequest>();
+            var data = EnterpriseEventCacheDataRequestFactory.CreateRequest(request.ToString()); 
             Logger.LogInformation(EventIds.ESSClearCacheSearchDownloadEventStart.ToEventId(), "Enterprise Event data deserialized in ESS and Data:{data} and _X-Correlation-ID:{correlationId}", JsonConvert.SerializeObject(data), GetCurrentCorrelationId());
            
             var validationResult = await essWebhookService.ValidateEventGridCacheDataRequest(data);
--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Models/Request/CustomEventGridEvent.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Models/Request/CustomEventGridEvent.cs
@@ -1,9 +1,33 @@
-﻿using Microsoft.Azure.EventGrid.Models;
+﻿//using Microsoft.Azure.EventGrid.Models   No Longer supported
+using Newtonsoft.Json;
 
-namespace UKHO.ExchangeSetService.Common.Models.Request
+namespace UKHO.ExchangeSetService.Common.Models.Request;
+
+// This class is not valid when using Azure.Messaging.EventGrid
+//public class CustomEventGridEvent  : EventGridEvent
+//"{"
+//    "public string Type { get; set; }"
+//"}"
+
+// This is Temporary while libraries are being updated
+//This will eventually be placed in its own file.
+public class DataRequestWrapper
 {
-    public class CustomEventGridEvent : EventGridEvent
+    public EnterpriseEventCacheDataRequest Data { get; set; }
+}
+
+public static class EnterpriseEventCacheDataRequestFactory
+{
+    class Wrapper
     {
-        public string Type { get; set; }
+        public EnterpriseEventCacheDataRequest Data { get; set; } = null;
     }
+
+    public static EnterpriseEventCacheDataRequest CreateRequest(string request)
+    {
+        var wrap = new Wrapper();
+        JsonConvert.PopulateObject(request, wrap);
+        return wrap.Data;
+    }
+    
 }
