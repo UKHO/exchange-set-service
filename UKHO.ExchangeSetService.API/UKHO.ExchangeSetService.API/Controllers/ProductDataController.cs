62188f6ce5d8b8547a73d09d06d94f7bd54ea08c
Pratibha Shinde <Pratibh14837@Mastek.Com>
2024-03-08 11:13:36 +0530

    AB#144895 - modified summary description for new endpoint

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/ProductDataController.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/ProductDataController.cs
@@ -289,15 +289,21 @@ namespace UKHO.ExchangeSetService.API.Controllers
                 }, GetCurrentCorrelationId(), exchangeSetStandard);
         }
 
-       
+
         /// <summary>
-        /// Get SCS response data after a datetime.
+        /// Provide all the releasable data after a datetime.
         /// </summary>
-        /// <remarks>Given a datetime, get an scs response data of all the releasable ENC versions that have been issued since that datetime.</remarks>
+        /// <remarks>Given a datetime, get all the releasable ENC versions that have been issued since that datetime.</remarks>
         /// <param name="sinceDateTime" example="Wed, 21 Oct 2015 07:28:00 GMT" >The date and time from which changes are requested. Any changes since the date will be returned. The value should be the value in the `Date` header returned by the last request to this operation. The date is in RFC 1123 format. The date and time must be within 28 days and cannot be in future.
         /// <br/><para><i>Example</i> : Wed, 21 Oct 2015 07:28:00 GMT</para>
         /// </param>
-        /// <response code="200">A JSON body that indicates the scs response. If there are no updates since the sinceDateTime parameter, then a 'Not modified' response will be returned.</response>
+        /// <response code="200">A JSON body that containing the information of ENC versions that have been issued since that datetime.</response>
+        /// <response code="304">Not modified.</response>
+        /// <response code="400">Bad Request.</response>
+        /// <response code="401">Unauthorised - either you have not provided any credentials, or your credentials are not recognised.</response>
+        /// <response code="403">Forbidden - you have been authorised, but you are not allowed to access this resource.</response>
+        /// <response code="429">You have sent too many requests in a given amount of time. Please back-off for the time in the Retry-After header (in seconds) and try again.</response>
+        /// <response code="500">Internal Server Error.</response>
 
         [HttpGet]
         [Route("/productData")]
