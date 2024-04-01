5cd70cfc575518e46ec4200be68c50927fde9e18
rushikeshkhopatkar <rushike12794@mastek.com>
2024-04-01 11:53:56 +0530

    Added additional condition in mock SCS Controller and handle AIODisabled scenarios

--- UKHO.SalesCatalogueFileShareServicesMock.API/UKHO.SalesCatalogueFileShareServicesMock.API/Controllers/SalesCatalogueServiceController.cs
+++ UKHO.SalesCatalogueFileShareServicesMock.API/UKHO.SalesCatalogueFileShareServicesMock.API/Controllers/SalesCatalogueServiceController.cs
@@ -65,6 +65,14 @@ namespace UKHO.SalesCatalogueFileShareServicesMock.API.Controllers
                     return Ok(response.ResponseBody);
                 }
             }
+            if (!productIdentifiers.Any())
+            {
+                var response = salesCatalogueService.GetProductIdentifier("productIdentifier-" + String.Join("-", productIdentifiers));
+                if (response != null)
+                {
+                    return Ok(response.ResponseBody);
+                }
+            }
             return BadRequest(new { CorrelationId = GetCurrentCorrelationId(), Errors = ErrorsIdentifiers });
         }
 
@@ -90,6 +98,14 @@ namespace UKHO.SalesCatalogueFileShareServicesMock.API.Controllers
                     isInitalIndex = false;
                 }
                 var response = salesCatalogueService.GetProductVersion("productVersion-" + productVersionRequestSearchText.ToString());
+                if (response != null)
+                {
+                    return Ok(response.ResponseBody);
+                }
+            }
+            if (!productVersionRequest.Any())
+            {
+                var response = salesCatalogueService.GetProductVersion("productVersion-");
                 if (response != null)
                 {
                     return Ok(response.ResponseBody);
