3aad3d9184fa93af2561394ae43b81fdc596807a
MattWin90 <matthew.winstanley@mastek.com>
2024-04-04 16:35:28 +0100

    added ui api definition

--- Deployment/src/Modules/APIM/variables.tf
+++ Deployment/src/Modules/APIM/variables.tf
@@ -40,6 +40,16 @@ variable "api_description" {
     default = "The Exchange Set Service APIs to request ENC Exchange Sets for loading onto an ECDIS."
 }
 
+variable "ui_api_name" {
+    type = string
+    default = "The Exchange Set Service UI API"
+}
+
+variable "ui_api_description" {
+    type = string
+    default = "The Exchange Set Service UI api to facilitate ESS UI Application Requests."
+}
+
 variable "ui_product_name" {
     type = string
     default = "Exchange Set Service UI"
@@ -126,7 +136,10 @@ locals {
   product_name          = local.env_name == "prod" ? "${var.product_name}${var.suffix}" : "${var.product_name} ${var.env_suffix[local.env_name]}${var.suffix}"
   ui_product_name       = local.env_name == "prod" ? "${var.ui_product_name}${var.suffix}" : "${var.ui_product_name} ${var.env_suffix[local.env_name]}${var.suffix}"
   api_name              = local.env_name == "prod" ? "${var.api_name}${var.suffix}" : "${var.api_name} ${var.env_suffix[local.env_name]}${var.suffix}"
+  ui_api_name           = local.env_name == "prod" ? "${var.ui_api_name}${var.suffix}" : "${var.ui_api_name} ${var.env_suffix[local.env_name]}${var.suffix}"
   apim_api_path         = local.env_name == "prod" ? "${local.service_name}${var.pathsuffix}" : "${local.service_name}-${local.env_name}${var.pathsuffix}"
+  apim_ui_api_path      = local.env_name == "prod" ? "${local.service_name}-ui-${var.pathsuffix}" : "${local.service_name}-ui-${local.env_name}${var.pathsuffix}"
   apim_api_openapi      = file("${path.module}/exchangeSetService_OpenApi_definition.yaml")
+  apim_ui_openapi      = file("${path.module}/exchangeSetService_UI_OpenApi_definition.yaml")
   cors_origins          = split(";", var.cors_origin_values)
 }
