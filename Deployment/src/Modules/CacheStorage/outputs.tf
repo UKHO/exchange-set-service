71ba9e0d9efc95f157d380db1f2f3c0c047525ad
tejasi15072 <tejasi15072@mastek.com>
2024-05-03 15:38:58 +0530

    updated terraform scripts AB#151914

--- Deployment/src/Modules/CacheStorage/outputs.tf
+++ Deployment/src/Modules/CacheStorage/outputs.tf
@@ -13,29 +13,29 @@ output cache_storage_primary_access_key {
 }
 
 output cache_storage1_name {
-  value = azurerm_storage_account.ess_cache_storage1.name
+  value = azurerm_storage_account.ess_cache_storage1.0.name
 }
 
 output cache_storage1_connection_string {
-  value = azurerm_storage_account.ess_cache_storage1.primary_connection_string
+  value = azurerm_storage_account.ess_cache_storage1.0.primary_connection_string
   sensitive = true
 }
 
 output cache_storage1_primary_access_key {
-  value = azurerm_storage_account.ess_cache_storage1.primary_access_key
+  value = azurerm_storage_account.ess_cache_storage1.0.primary_access_key
   sensitive = true
 }
 
 output cache_storage2_name {
-  value = azurerm_storage_account.ess_cache_storage2.name
+  value = azurerm_storage_account.ess_cache_storage2.0.name
 }
 
 output cache_storage2_connection_string {
-  value = azurerm_storage_account.ess_cache_storage2.primary_connection_string
+  value = azurerm_storage_account.ess_cache_storage2.0.primary_connection_string
   sensitive = true
 }
 
 output cache_storage2_primary_access_key {
-  value = azurerm_storage_account.ess_cache_storage2.primary_access_key
+  value = azurerm_storage_account.ess_cache_storage2.0.primary_access_key
   sensitive = true
 }
