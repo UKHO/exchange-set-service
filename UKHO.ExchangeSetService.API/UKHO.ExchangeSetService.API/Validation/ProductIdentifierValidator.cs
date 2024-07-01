8f1431a47953bafbd03ee7a090050cd89fc0ca0b
Daniel Owen <adanielo@Engineering.ukho.gov.uk>
2024-07-01 13:28:49 +0000

    Fix

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Validation/ProductIdentifierValidator.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Validation/ProductIdentifierValidator.cs
@@ -23,7 +23,7 @@ namespace UKHO.ExchangeSetService.API.Validation
                .WithMessage("productIdentifiers cannot be null or empty.");
             
             RuleFor(p => p.ProductIdentifier)
-                .Must(pi => pi != null)
+                .Must(pi => pi == null || pi.Length != 0)
                 .WithErrorCode(HttpStatusCode.BadRequest.ToString())
                 .WithMessage("Either body is null or malformed.");
 
