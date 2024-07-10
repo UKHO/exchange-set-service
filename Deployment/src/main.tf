d7822c3ec7f4ae83dac1541ea493f3368ebf0fc1
pkent81 <130072235+pkent81@users.noreply.github.com>
2024-07-10 12:00:02 +0100

    Added subnet variables

--- Deployment/src/main.tf
+++ Deployment/src/main.tf
@@ -140,6 +140,8 @@ module "key_vault" {
   allowed_ips         = var.allowed_ips  
   subnet_id           = data.azurerm_subnet.main_subnet.id
   agent_subnet        = data.azurerm_subnet.agent_subnet.id
+  agent_2204_subnet   = var.agent_2204_subnet
+  agent_prd_subnet    = var.agent_prd_subnet
   read_access_objects = {
     "ess_service_identity" = module.user_identity.ess_service_identity_principal_id   
   }
