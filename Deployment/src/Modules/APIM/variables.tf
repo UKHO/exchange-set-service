d9ef3bb861de15c13fe54ec0ab22a887cbd0c681
DanOwenUKHO <118909036+DanOwenUKHO@users.noreply.github.com>
2024-08-12 15:06:42 +0000

    Fix suffix

--- Deployment/src/Modules/APIM/variables.tf
+++ Deployment/src/Modules/APIM/variables.tf
@@ -146,7 +146,7 @@ locals {
   api_name              = local.env_name == "prod" ? "${var.api_name}${var.suffix}" : "${var.api_name} ${var.env_suffix[local.env_name]}${var.suffix}"
   apim_api_path         = local.env_name == "prod" ? "${local.service_name}${var.pathsuffix}" : "${local.service_name}-${local.env_name}${var.pathsuffix}"
   ui_api_name           = local.env_name == "prod" ? "${var.ui_api_name}${var.suffix}" : "${var.ui_api_name} ${var.env_suffix[local.env_name]}${var.suffix}"
-  apim_ui_api_path      = local.env_name == "prod" ? "${local.service_name}-ui${var.suffix}" : "${local.service_name}-ui-${local.env_name}${var.pathsuffix}"
+  apim_ui_api_path      = local.env_name == "prod" ? "${local.service_name}-ui${var.pathsuffix}" : "${local.service_name}-ui-${local.env_name}${var.pathsuffix}"
 
   apim_api_openapi      = file("${path.module}/exchangeSetService_OpenApi_definition.yaml")
   apim_ui_openapi       = file("${path.module}/exchangeSetService_Ui_OpenApi_definition.yaml")
