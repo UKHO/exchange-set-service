bce0c9c7394357705ec296c49067113e9d161c05
Rashi14748 <76434249+Rashi14748@users.noreply.github.com>
2024-08-21 13:56:55 +0530

    AB#164861 reverted quota limit

--- Deployment/src/Modules/AppInsights/main.tf
+++ Deployment/src/Modules/AppInsights/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_application_insights" "app_insights" {
   location             = var.location
   resource_group_name  = var.resource_group_name
   application_type     = "web"
-  daily_data_cap_in_gb = 10
+  daily_data_cap_in_gb = 5
   workspace_id         = azurerm_log_analytics_workspace.logs_analytics_workspace.id
   tags                 = var.tags
 }
