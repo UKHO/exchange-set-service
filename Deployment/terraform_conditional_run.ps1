e9d4ef2748af8cf68cd0f06cf5dac5e4d7778169
HugoBurgess <hugo.burgess@ukho.gov.uk>
2024-09-24 15:23:19 +0100

    Display Terraform version

--- Deployment/terraform_conditional_run.ps1
+++ Deployment/terraform_conditional_run.ps1
@@ -10,6 +10,8 @@ param (
 
 cd $env:AGENT_BUILDDIRECTORY/terraformartifact/src
 
+terraform --version
+
 Write-output "Executing terraform scripts for deployment in $workSpace enviroment"
 terraform init -backend-config="resource_group_name=$deploymentResourceGroupName" -backend-config="storage_account_name=$deploymentStorageAccountName" -backend-config="key=terraform.deployment.tfplan"
 if ( !$? ) { echo "Something went wrong during terraform initialization"; throw "Error" }
