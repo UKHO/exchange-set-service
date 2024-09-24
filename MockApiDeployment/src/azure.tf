627416933f982fa4eafc46f2fe61bdd8d8afba06
HugoBurgess <hugo.burgess@ukho.gov.uk>
2024-09-24 15:11:07 +0100

    Update Terraform

--- MockApiDeployment/src/azure.tf
+++ MockApiDeployment/src/azure.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.89.0"
+      version = "=3.116.0"
     }
   }
 
-  required_version = "=1.7.2"
+  required_version = "=1.9.6"
   backend "azurerm" {
     container_name = "tfstate"
     key            = "mockapiterraform.deployment.tfplan"
