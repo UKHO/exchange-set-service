87ead922d2d56e54dc4af6ad13cca6e2dca57985
mayur1501364 <127309264+mayur1501364@users.noreply.github.com>
2024-12-17 12:05:18 +0530

    194402- base controller added AB#194402 AB#194756

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/ExchangeSetController.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/ExchangeSetController.cs
@@ -1,13 +1,21 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace UKHO.ExchangeSetService.API.Controllers
 {
     [Route("v2/exchangeSet")]
-    public class ExchangeSetController : ControllerBase
+    public class ExchangeSetController : ExchangeSetControllerBase<ExchangeSetController>
     {
+        private readonly ILogger<ExchangeSetController> _logger;
 
+        public ExchangeSetController(IHttpContextAccessor httpContextAccessor,ILogger<ExchangeSetController> logger) : base(httpContextAccessor)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
     }
 }
---
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/ExchangeSetControllerBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UKHO.ExchangeSetService.API.Controllers
+{
+    public abstract class ExchangeSetControllerBase<T> : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public const string XCorrelationIdHeaderKey = "X-Correlation-ID";
+
+        protected ExchangeSetControllerBase(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// Get Correlation Id.
+        /// </summary>
+        /// <remarks>
+        /// Correlation Id is Guid based id to track request.
+        /// Correlation Id can be found in request headers.
+        /// </remarks>
+        /// <returns>Correlation Id</returns>
+        protected string GetCorrelationId()
+        {
+            return _httpContextAccessor.HttpContext!.Request.Headers[XCorrelationIdHeaderKey].FirstOrDefault()!;
+        }
+    }
+}
