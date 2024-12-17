ba15fd2edefd7f09831f22c4e0f871fa27fea43b
mayur1501364 <127309264+mayur1501364@users.noreply.github.com>
2024-12-17 13:26:40 +0530

    194402 - unit test cases fixed AB#194402 AB#194756

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Controllers/ExchangeSetControllerTests.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Controllers/ExchangeSetControllerTests.cs
@@ -1,10 +1,11 @@
-﻿
-using System;
+﻿using System;
 using FakeItEasy;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using UKHO.ExchangeSetService.API.Controllers;
+using UKHO.ExchangeSetService.API.Services;
 
 namespace UKHO.ExchangeSetService.API.UnitTests.Controllers
 {
@@ -12,19 +13,25 @@ namespace UKHO.ExchangeSetService.API.UnitTests.Controllers
     public class ExchangeSetControllerTests
     {
         private IHttpContextAccessor _fakeHttpContextAccessor;
+        private IExchangeSetService _fakeExchangeSetService;
+        private ILogger<ExchangeSetController> _fakeLogger;
 
         [SetUp]
         public void Setup()
         {
             _fakeHttpContextAccessor = A.Fake<IHttpContextAccessor>();
-
+            _fakeExchangeSetService = A.Fake<IExchangeSetService>();
+            _fakeLogger = A.Fake<ILogger<ExchangeSetController>>();
         }
 
         [Test]
         public void WhenParameterIsNull_ThenConstructorThrowsArgumentNullException()
         {
-            Action nullLogger = () => new ExchangeSetController(_fakeHttpContextAccessor, null);
+            Action nullLogger = () => new ExchangeSetController(_fakeHttpContextAccessor, null, _fakeExchangeSetService);
             nullLogger.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("logger");
+
+            Action nullExchangeSetService = () => new ExchangeSetController(_fakeHttpContextAccessor, _fakeLogger, null);
+            nullExchangeSetService.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("exchangeSetService");
         }
     }
 }
