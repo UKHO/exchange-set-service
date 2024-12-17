134176ad8dd1866a7215c46d420ad0dfb3d9a718
Nadeem Badai <Nadeem14456@Mastek.Com>
2024-12-17 11:05:26 +0530

    194402 - Remove ProductIdentifiers endpoint and related usings. AB#194772

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/ExchangeSetController.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/ExchangeSetController.cs
@@ -1,27 +1,13 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Net;
 using Microsoft.AspNetCore.Mvc;
-using UKHO.ExchangeSetService.Common.Models;
-using UKHO.ExchangeSetService.Common.Models.Enums;
-using UKHO.ExchangeSetService.Common.Models.Response;
 
 namespace UKHO.ExchangeSetService.API.Controllers
 {
     [Route("v2/exchangeSet")]
     public class ExchangeSetController : ControllerBase
     {
-        [HttpPost("{standard}/productIdentifiers")]
-        public IActionResult ProductIdentifiers(ExchangeSetStandard exchangeSetStandard, [FromBody] string[] productIdentifiers, [FromQuery] string callbackUri)
-        {
-            var result = ExchangeSetServiceResponseResult<ExchangeSetResponse>.Success(new ExchangeSetResponse()); // This is a placeholder, the actual implementation is not provided
-            return result.StatusCode switch
-            {
-                HttpStatusCode.OK => Ok(result.Value),
-                HttpStatusCode.BadRequest => BadRequest(result.ErrorResponse),
-                _ => StatusCode((int)result.StatusCode)
-            };
-        }
+
     }
 }
