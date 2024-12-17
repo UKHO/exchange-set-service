2e9f74ffaf94ef834b820e79e4e6134e2ee4d2e5
mayur1501364 <127309264+mayur1501364@users.noreply.github.com>
2024-12-17 12:32:54 +0530

    194402 - Added unit test case AB#194402 AB#194756

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Controllers/ExchangeSetControllerTests.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Controllers/ExchangeSetControllerTests.cs
@@ -1,4 +1,8 @@
 ﻿
+using System;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
 using NUnit.Framework;
 using UKHO.ExchangeSetService.API.Controllers;
 
@@ -7,12 +11,20 @@ namespace UKHO.ExchangeSetService.API.UnitTests.Controllers
     [TestFixture]
     public class ExchangeSetControllerTests
     {
-        private ExchangeSetController _fakeExchangeSetController;
+        private IHttpContextAccessor _fakeHttpContextAccessor;
 
         [SetUp]
         public void Setup()
         {
-            _fakeExchangeSetController = new ExchangeSetController();
+            _fakeHttpContextAccessor = A.Fake<IHttpContextAccessor>();
+
+        }
+
+        [Test]
+        public void WhenParameterIsNull_ThenConstructorThrowsArgumentNullException()
+        {
+            Action nullLogger = () => new ExchangeSetController(_fakeHttpContextAccessor, null);
+            nullLogger.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("logger");
         }
     }
 }
