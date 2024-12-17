bc4c7a7c346bff7dfd4d64a44bb84988bd9757b9
mayur1501364 <127309264+mayur1501364@users.noreply.github.com>
2024-12-17 11:12:55 +0530

    194402 - Added Unit test class for controller AB#194756

---
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Controllers/ExchangeSetControllerTests.cs
@@ -0,0 +1,18 @@
+﻿
+using NUnit.Framework;
+using UKHO.ExchangeSetService.API.Controllers;
+
+namespace UKHO.ExchangeSetService.API.UnitTests.Controllers
+{
+    [TestFixture]
+    public class ExchangeSetControllerTests
+    {
+        private ExchangeSetController _fakeExchangeSetController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fakeExchangeSetController = new ExchangeSetController();
+        }
+    }
+}
