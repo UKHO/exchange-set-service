07bc5af5c11aa853a8fb114f362255b5b783698d
mayur1501364 <127309264+mayur1501364@users.noreply.github.com>
2024-12-18 18:50:26 +0530

    194402 - Model changes AB#194402

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Models/ExchangeSetServiceResponseResult.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Models/ExchangeSetServiceResponseResult.cs
@@ -2,21 +2,22 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Net;
+using UKHO.ExchangeSetService.Common.Models.Response;
 
 namespace UKHO.ExchangeSetService.Common.Models
 {
     public class ExchangeSetServiceResponseResult<ExchangeSetResponse> : Result<ExchangeSetResponse>
     {
-        public new ErrorResponse ErrorResponse { get; }
+        public new ErrorDescription ErrorDescription { get; }
 
-        private ExchangeSetServiceResponseResult(ExchangeSetResponse value, HttpStatusCode statusCode, ErrorResponse errorResponse = null)
-            : base(value, statusCode, errorResponse)
+        private ExchangeSetServiceResponseResult(ExchangeSetResponse value, HttpStatusCode statusCode, ErrorDescription errorDescription = null)
+            : base(value, statusCode, errorDescription)
         {
-            ErrorResponse = errorResponse;
+            ErrorDescription = errorDescription;
         }
 
         public static ExchangeSetServiceResponseResult<ExchangeSetResponse> Success(ExchangeSetResponse value) => new(value, HttpStatusCode.Accepted);
 
-        public static ExchangeSetServiceResponseResult<ExchangeSetResponse> BadRequest(ErrorResponse errorResponse) => new(default, HttpStatusCode.BadRequest, errorResponse);
+        public static ExchangeSetServiceResponseResult<ExchangeSetResponse> BadRequest(ErrorDescription errorDescription) => new(default, HttpStatusCode.BadRequest, errorDescription);
     }
 }
