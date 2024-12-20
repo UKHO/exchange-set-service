d9fb6d7b67138ad336a992f17499de296ec744a0
Zeba Dhongre <zeba15024@mastek.com>
2024-12-20 15:01:12 +0530

    194476- UTs for product versions api endpoint AB#194476 AB#194787

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Controllers/ExchangeSetControllerTests.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Controllers/ExchangeSetControllerTests.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
 using FakeItEasy;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using UKHO.ExchangeSetService.API.Controllers;
 using UKHO.ExchangeSetService.API.Services;
+using UKHO.ExchangeSetService.Common.Models.Response;
+using UKHO.ExchangeSetService.Common.Models;
+using UKHO.ExchangeSetService.Common.Models.V2.Request;
+using System.Collections.Generic;
 
 namespace UKHO.ExchangeSetService.API.UnitTests.Controllers
 {
@@ -16,12 +23,16 @@ namespace UKHO.ExchangeSetService.API.UnitTests.Controllers
         private IExchangeSetService _fakeExchangeSetService;
         private ILogger<ExchangeSetController> _fakeLogger;
 
+        private ExchangeSetController _controller;
+
         [SetUp]
         public void Setup()
         {
             _fakeHttpContextAccessor = A.Fake<IHttpContextAccessor>();
             _fakeExchangeSetService = A.Fake<IExchangeSetService>();
             _fakeLogger = A.Fake<ILogger<ExchangeSetController>>();
+
+            _controller = new ExchangeSetController(_fakeHttpContextAccessor, _fakeLogger, _fakeExchangeSetService);
         }
 
         [Test]
@@ -33,5 +44,43 @@ namespace UKHO.ExchangeSetService.API.UnitTests.Controllers
             Action nullExchangeSetService = () => new ExchangeSetController(_fakeHttpContextAccessor, _fakeLogger, null);
             nullExchangeSetService.Should().ThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("exchangeSetService");
         }
+
+        #region ExchangeSetStandardProductVersions
+
+        [Test]
+        public async Task WhenExchangeSetStandardProductVersionsRequested_ThenPostProductVersions_Returns202Accepted()
+        {
+            var productVersionRequest = new List<ProductVersionRequest>
+            {
+                new ProductVersionRequest { ProductName = "DE416080", EditionNumber = 9, UpdateNumber = 1 },
+                new ProductVersionRequest { ProductName = "DE416081", EditionNumber = 10, UpdateNumber = 2 },
+            };
+
+            A.CallTo(() => _fakeExchangeSetService.CreateExchangeSetByProductVersions(A<ProductVersionsRequest>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(Task.FromResult(ServiceResponseResult<ExchangeSetResponse>.Accepted(new ExchangeSetResponse())));
+
+            var result = await _controller.PostProductVersions(productVersionRequest, "https://callback.uri", "s100");
+
+            result.Should().BeOfType<ObjectResult>().Which.StatusCode.Should().Be(StatusCodes.Status202Accepted);
+        }
+
+        [Test]
+        public async Task WhenInvalidExchangeSetStandardProductVersionsRequested_ThenPostProductVersions_ReturnsBadRequest()
+        {
+            var productVersionRequest = new List<ProductVersionRequest>
+            {
+                new ProductVersionRequest { ProductName = "", EditionNumber = 9, UpdateNumber = 1 },
+                new ProductVersionRequest { ProductName = "", EditionNumber = 10, UpdateNumber = 2 },
+            };
+
+            A.CallTo(() => _fakeExchangeSetService.CreateExchangeSetByProductVersions(A<ProductVersionsRequest>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(Task.FromResult(ServiceResponseResult<ExchangeSetResponse>.BadRequest(new ErrorDescription { CorrelationId = Guid.NewGuid().ToString(), Errors = new List<Error> { new() { Source = "test", Description = "test error" } } })));
+
+            var result = await _controller.PostProductVersions(productVersionRequest, "https://callback.uri", "s100");
+
+            result.Should().BeOfType<BadRequestObjectResult>().Which.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+        }
+
+        #endregion ExchangeSetStandardProductVersions
     }
 }
