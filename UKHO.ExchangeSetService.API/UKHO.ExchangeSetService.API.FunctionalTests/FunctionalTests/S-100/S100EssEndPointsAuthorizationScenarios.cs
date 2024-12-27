9f3dab30a53452845dc52fb0bc724508372cb80d
Shivam Verma <shivam15622@mastek.com>
2024-12-27 15:39:19 +0530

    AB#194403 - Uplifted code AB#194403

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/FunctionalTests/S-100/S100EssEndPointsAuthorizationScenarios.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/FunctionalTests/S-100/S100EssEndPointsAuthorizationScenarios.cs
@@ -4,6 +4,7 @@ using System;
 using System.Threading.Tasks;
 using UKHO.ExchangeSetService.API.FunctionalTests.Helper;
 using UKHO.ExchangeSetService.API.FunctionalTests.Models;
+using System.Collections.Generic;
 
 namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests.S_100
 {
@@ -11,6 +12,7 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests.S_100
     {
         private string sinceDateTime;
         private UpdatesSinceModel sinceDateTimePayload;
+        private List<string> productNames;
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
@@ -24,6 +26,7 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests.S_100
                 DataHelper.GetProductVersionModelData("104US00_CHES_TYPE1_20210630_0600", 7, 10),
                 DataHelper.GetProductVersionModelData("111US00_ches_dcf8_20190703T00Z", 36, 0)
             ];
+            productNames = DataHelper.GetProductNamesForS100();
         }
 
         //PBI 194403: Azure AD Authorization
@@ -49,7 +52,6 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests.S_100
         [Category("QCOnlyTest-AIOEnabled")]
         public async Task WhenICallS100ProductNamesEndPointWithValidToken_ThenResponseCodeReturnedIs202Accepted()
         {
-            var productNames = DataHelper.GetProductNamesForS100();
             var apiResponse = await ExchangeSetApiClient.GetProductIdentifiersDataAsync(productNames, null, EssJwtToken, "s100");
             Assert.That((int)apiResponse.StatusCode, Is.EqualTo(202), $"Incorrect status code is returned {apiResponse.StatusCode}, instead of the expected 202.");
         }
@@ -59,7 +61,6 @@ namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests.S_100
         [Category("QCOnlyTest-AIOEnabled")]
         public async Task WhenICallS100ProductNamesEndPointWithInvalidToken_ThenResponseCodeReturnedIs401Unauthorized()
         {
-            var productNames = DataHelper.GetProductNamesForS100();
             var apiResponse = await ExchangeSetApiClient.GetProductIdentifiersDataAsync(productNames, null, "InvalidEssJwtToken", "s100");
             Assert.That((int)apiResponse.StatusCode, Is.EqualTo(401), $"Incorrect status code is returned {apiResponse.StatusCode}, instead of the expected 401.");
         }
