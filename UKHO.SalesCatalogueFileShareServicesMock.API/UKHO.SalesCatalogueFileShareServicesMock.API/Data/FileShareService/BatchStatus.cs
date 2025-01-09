ca51fed34dd721abea29a284ce18845841d58a24
Shailendra Shinde <shailen14599@mastek.com>
2025-01-09 13:26:12 +0530

    194749 - Added code changes for empty incomplete batch AB#194749 AB#196277

---
+++ UKHO.SalesCatalogueFileShareServicesMock.API/UKHO.SalesCatalogueFileShareServicesMock.API/Data/FileShareService/BatchStatus.cs
@@ -0,0 +1,12 @@
+﻿namespace UKHO.SalesCatalogueFileShareServicesMock.API.Data.FileShareService
+{
+    public enum BatchStatus
+    {
+        Incomplete = 1,
+        CommitInProgress = 2,
+        Committed = 3,
+        Rolledback = 4,
+        Failed = 5,
+        Deleted = 6
+    }
+}
