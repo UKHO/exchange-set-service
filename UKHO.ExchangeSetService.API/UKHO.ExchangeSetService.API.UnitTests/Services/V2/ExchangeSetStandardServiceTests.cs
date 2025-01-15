80a123e4301abad4ffdc34b9063eafb2311c006d
Zeba Dhongre <zeba15024@mastek.com>
2025-01-15 20:31:09 +0530

    194749 - ut for create fss incomplete batch for product version AB#194749 AB#196278

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Services/V2/ExchangeSetStandardServiceTests.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.UnitTests/Services/V2/ExchangeSetStandardServiceTests.cs
@@ -242,9 +242,11 @@ namespace UKHO.ExchangeSetService.API.UnitTests.Services.V2
             string[] productNames = { "Product1", "Product2" };
 
             A.CallTo(() => _fakeProductNameValidator.Validate(A<ProductNameRequest>.Ignored)).Returns(new ValidationResult());
+
             A.CallTo(() => _fakeSalesCatalogueService.PostProductNamesAsync(A<ApiVersion>.Ignored, A<string>.Ignored, A<IEnumerable<string>>.Ignored, A<string>.Ignored, A<CancellationToken>.Ignored))
                 .Returns(ServiceResponseResult<SalesCatalogueResponse>
                 .Success(GetSalesCatalogueResponse()));
+
             A.CallTo(() => _fakeFileShareService.CreateBatch(A<string>.Ignored, A<string>.Ignored))
                 .Returns(GetCreateBatchResponse());
 
@@ -260,37 +262,16 @@ namespace UKHO.ExchangeSetService.API.UnitTests.Services.V2
         }
 
         [Test]
-        public async Task WhenProductNamesCreateBatchResponseIsCreated_ThenProcessProductNamesShouldReturnAccepted()
+        public async Task WhenProductNamesFSSBatchResponseIsNotCreated_ThenProcessProductNamesShouldReturnInternalServerError()
         {
             string[] productNames = { "Product1" };
 
             A.CallTo(() => _fakeProductNameValidator.Validate(A<ProductNameRequest>.Ignored)).Returns(new ValidationResult());
+
             A.CallTo(() => _fakeSalesCatalogueService.PostProductNamesAsync(A<ApiVersion>.Ignored, A<string>.Ignored, A<IEnumerable<string>>.Ignored, A<string>.Ignored, A<CancellationToken>.Ignored))
                 .Returns(ServiceResponseResult<SalesCatalogueResponse>
                 .Success(GetSalesCatalogueResponse()));
-            A.CallTo(() => _fakeFileShareService.CreateBatch(A<string>.Ignored, A<string>.Ignored))
-                .Returns(GetCreateBatchResponse());
 
-            var result = await _service.ProcessProductNamesRequestAsync(productNames, ApiVersion.V2, ExchangeSetStandard.s100.ToString(), CallbackUri, _fakeCorrelationId, CancellationToken.None);
-
-            result.Should().NotBeNull();
-            result.StatusCode.Should().Be(HttpStatusCode.Accepted);
-
-            A.CallTo(_fakeLogger).Where(call => call.Method.Name == "Log"
-            && call.GetArgument<LogLevel>(0) == LogLevel.Error
-            && call.GetArgument<EventId>(1) == EventIds.ValidationFailed.ToEventId()
-            && call.GetArgument<IEnumerable<KeyValuePair<string, object>>>(2)!.ToDictionary(c => c.Key, c => c.Value)["{OriginalFormat}"].ToString() == "Validation failed for {RequestType} | errors : {errors} | _X-Correlation-ID : {correlationId}").MustNotHaveHappened();
-        }
-
-        [Test]
-        public async Task WhenProductNamesCreateBatchResponseIsNotCreated_ThenProcessProductNamesShouldReturnInternalServerError()
-        {
-            string[] productNames = { "Product1" };
-
-            A.CallTo(() => _fakeProductNameValidator.Validate(A<ProductNameRequest>.Ignored)).Returns(new ValidationResult());
-            A.CallTo(() => _fakeSalesCatalogueService.PostProductNamesAsync(A<ApiVersion>.Ignored, A<string>.Ignored, A<IEnumerable<string>>.Ignored, A<string>.Ignored, A<CancellationToken>.Ignored))
-                .Returns(ServiceResponseResult<SalesCatalogueResponse>
-                .Success(GetSalesCatalogueResponse()));
             A.CallTo(() => _fakeFileShareService.CreateBatch(A<string>.Ignored, A<string>.Ignored))
                 .Returns(new CreateBatchResponse { ResponseCode = HttpStatusCode.BadRequest });
 
@@ -298,6 +279,26 @@ namespace UKHO.ExchangeSetService.API.UnitTests.Services.V2
 
             result.Should().NotBeNull();
             result.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+
+            A.CallTo(_fakeLogger).Where(call => call.Method.Name == "Log"
+            && call.GetArgument<LogLevel>(0) == LogLevel.Error
+            && call.GetArgument<EventId>(1) == EventIds.ValidationFailed.ToEventId()
+            && call.GetArgument<IEnumerable<KeyValuePair<string, object>>>(2)!.ToDictionary(c => c.Key, c => c.Value)["{OriginalFormat}"].ToString() == "Validation failed for {RequestType} | errors : {errors} | _X-Correlation-ID : {correlationId}").MustNotHaveHappened();
+        }
+
+        [Test]
+        public async Task WhenSalesCatalogueServiceReturnsBadRequest_ThenProcessProductNamesRequestAsyncReturnsBadRequest()
+        {
+            string[] productNames = { "Product1" };
+
+            var salesCatalogueResponse = ServiceResponseResult<SalesCatalogueResponse>.BadRequest(GetBadRequestResponse());
+
+            A.CallTo(() => _fakeSalesCatalogueService.PostProductNamesAsync(A<ApiVersion>.Ignored, A<string>.Ignored, A<IEnumerable<string>>.Ignored, A<string>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(salesCatalogueResponse);
+
+            var result = await _service.ProcessProductNamesRequestAsync(productNames, ApiVersion.V2, ExchangeSetStandard.s100.ToString(), CallbackUri, _fakeCorrelationId, CancellationToken.None);
+
+            result.StatusCode.Should().Be(HttpStatusCode.BadRequest);
 
             A.CallTo(_fakeLogger).Where(call => call.Method.Name == "Log"
             && call.GetArgument<LogLevel>(0) == LogLevel.Error
@@ -376,18 +377,8 @@ namespace UKHO.ExchangeSetService.API.UnitTests.Services.V2
             A.CallTo(() => _fakeProductVersionsValidator.Validate(A<ProductVersionsRequest>.Ignored))
                 .Returns(new ValidationResult());
 
-            var errorDescription = new ErrorDescription
-            {
-                CorrelationId = _fakeCorrelationId,
-                Errors = [new Error
-                {
-                    Description = "Error in sales catalogue service",
-                    Source = "Sales catalogue service"
-                }]
-            };
-
             A.CallTo(() => _fakeSalesCatalogueService.PostProductVersionsAsync(A<ApiVersion>.Ignored, A<string>.Ignored, A<IEnumerable<ProductVersionRequest>>.Ignored, A<string>.Ignored, A<CancellationToken>.Ignored))
-                .Returns(ServiceResponseResult<SalesCatalogueResponse>.BadRequest(errorDescription));
+                .Returns(ServiceResponseResult<SalesCatalogueResponse>.BadRequest(GetBadRequestResponse()));
 
             var result = await _service.ProcessProductVersionsRequestAsync(productVersions, ApiVersion.V2, ExchangeSetStandard.s100.ToString(), CallbackUri, _fakeCorrelationId, CancellationToken.None);
 
@@ -474,6 +465,34 @@ namespace UKHO.ExchangeSetService.API.UnitTests.Services.V2
             && call.GetArgument<IEnumerable<KeyValuePair<string, object>>>(2)!.ToDictionary(c => c.Key, c => c.Value)["{OriginalFormat}"].ToString() == "Either body is null or malformed | _X-Correlation-ID : {correlationId}").MustHaveHappened();
         }
 
+        [Test]
+        public async Task WhenProductVersionsFSSBatchResponseIsNotCreated_ThenProcessProductVersionsRequestReturnsInternalServerError()
+        {
+            var productVersions = new List<ProductVersionRequest>
+            {
+                new () { ProductName = "101GB40079ABCDEFG", EditionNumber = 7, UpdateNumber = 10 }
+            };
+
+            A.CallTo(() => _fakeProductVersionsValidator.Validate(A<ProductVersionsRequest>.Ignored))
+                .Returns(new ValidationResult());
+
+            A.CallTo(() => _fakeSalesCatalogueService.PostProductVersionsAsync(A<ApiVersion>.Ignored, A<string>.Ignored, A<IEnumerable<ProductVersionRequest>>.Ignored, A<string>.Ignored, A<CancellationToken>.Ignored))
+                .Returns(ServiceResponseResult<SalesCatalogueResponse>.Success(GetSalesCatalogueServiceResponseForProductVersions(HttpStatusCode.OK)));
+
+            A.CallTo(() => _fakeFileShareService.CreateBatch(A<string>.Ignored, A<string>.Ignored))
+                .Returns(new CreateBatchResponse { ResponseCode = HttpStatusCode.BadRequest });
+
+            var result = await _service.ProcessProductVersionsRequestAsync(productVersions, ApiVersion.V2, ExchangeSetStandard.s100.ToString(), CallbackUri, _fakeCorrelationId, CancellationToken.None);
+
+            result.Should().NotBeNull();
+            result.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+
+            A.CallTo(_fakeLogger).Where(call => call.Method.Name == "Log"
+            && call.GetArgument<LogLevel>(0) == LogLevel.Error
+            && call.GetArgument<EventId>(1) == EventIds.ValidationFailed.ToEventId()
+            && call.GetArgument<IEnumerable<KeyValuePair<string, object>>>(2)!.ToDictionary(c => c.Key, c => c.Value)["{OriginalFormat}"].ToString() == "Validation failed for {RequestType} | errors : {errors} | _X-Correlation-ID : {correlationId}").MustNotHaveHappened();
+        }
+       
         private ValidationResult GetValidationResult()
         {
             return new ValidationResult(
@@ -601,6 +620,18 @@ namespace UKHO.ExchangeSetService.API.UnitTests.Services.V2
                     BatchExpiryDateTime = "2025-02-17T16:19:32.269Z"
                 },
                 ResponseCode = HttpStatusCode.Created
+            };
+        }
+
+        private ErrorDescription GetBadRequestResponse()
+        {
+            return new ErrorDescription
+            {
+                CorrelationId = _fakeCorrelationId,
+                Errors = new List<Error>
+                {
+                    new Error { Description = "Error in sales catalogue service", Source = "Sales catalogue service" }
+                }
             };
         }
     }
