9d13db485da423333e72c44362b04e05043d664a
HugoBurgess <hugo.burgess@ukho.gov.uk>
2025-02-12 12:04:47 +0000

    And large

--- Deployment/src/Modules/FulfilmentWebapps/main.tf
+++ Deployment/src/Modules/FulfilmentWebapps/main.tf
@@ -179,6 +179,7 @@ resource "azurerm_app_service_plan" "large_exchange_set_app_service_plan" {
 	size = var.app_service_sku.size
   }
   tags                = var.tags
+  zone_redundant      = true
 }
 
 resource "azurerm_app_service" "large_exchange_set_webapp" {
