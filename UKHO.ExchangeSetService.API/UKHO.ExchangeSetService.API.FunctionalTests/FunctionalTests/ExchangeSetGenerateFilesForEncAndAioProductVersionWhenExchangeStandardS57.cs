fb03f8d7776dab67f111df2ab702a4dd83b394f6
Jivitesh Trivedi <ajivitesh@ukho.gov.uk>
2025-02-24 11:32:16 +0000

    Add functional tests for AIO file presence in exchange sets
    Introduced ExchangeSetGenerateFilesForEncAndAioProductVersionWhenExchangeStandardS57 class in UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests namespace. This class inherits from ObjectStorage and includes setup and teardown methods for functional tests. Added two test methods to verify the presence and absence of a specific serial AIO file in downloaded exchange sets. SetupAsync method initializes configurations, clients, and tokens, and prepares product version data. GlobalTeardown method cleans up downloaded files and folders.

---
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/FunctionalTests/ExchangeSetGenerateFilesForEncAndAioProductVersionWhenExchangeStandardS57.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UKHO.ExchangeSetService.API.FunctionalTests.Helper;
+using UKHO.ExchangeSetService.API.FunctionalTests.Models;
+
+namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
+{
+    public class ExchangeSetGenerateFilesForEncAndAioProductVersionWhenExchangeStandardS57 : ObjectStorage
+    {
+        [OneTimeSetUp]
+        public async Task SetupAsync()
+        {
+            Config = new TestConfiguration();
+            ExchangeSetApiClient = new ExchangeSetApiClient(Config.EssBaseAddress);
+            AuthTokenProvider authTokenProvider = new AuthTokenProvider();
+            EssJwtToken = await authTokenProvider.GetEssToken();
+            FssJwtToken = await authTokenProvider.GetFssToken();
+            ScsApiClient = new SalesCatalogueApiClient(Config.ScsAuthConfig.BaseUrl);
+            ScsJwtToken = await authTokenProvider.GetScsToken();
+            FssApiClient = new FssApiClient();
+            DataHelper = new DataHelper();
+            ProductVersionData = new List<ProductVersionModel>();
+            ProductVersionData.Add(DataHelper.GetProductVersionModelData(Config.AIOConfig.AioCellName, Config.AIOConfig.AioEditionNumber, 0));
+            ProductVersionData.Add(DataHelper.GetProductVersionModelData("DE4NO18Q", 2, 0));
+            ApiEssResponse = await ExchangeSetApiClient.GetProductVersionsAsync(ProductVersionData, accessToken: EssJwtToken, callbackUri: null, exchangeSetStandard: "s57");
+            //Get the BatchId
+            batchId = await ApiEssResponse.GetBatchId();
+            DownloadedFolderPath = await FileContentHelper.DownloadAndExtractAioZip(ApiEssResponse, FssJwtToken);
+        }
+
+        
+        [Test]
+        public void WhenIDownloadAioZipExchangeSet_ThenASerialAioFileIsAvailable()
+        {
+            bool checkFile = FssBatchHelper.CheckforFileExist(DownloadedFolderPath, Config.AIOConfig.ExchangeSetSerialAioFile);
+            Assert.That(checkFile, Is.True, $"{Config.ExchangeSetSerialEncFile} File not Exist in the specified folder path : {DownloadedFolderPath}");
+        }
+
+        [Test]
+        public void WhenIDownloadZipExchangeSet_ThenASerialAioFileIsNotAvailable()
+        {
+            bool checkFile = FssBatchHelper.CheckforFileExist(DownloadedFolderPath, Config.AIOConfig.ExchangeSetSerialAioFile);
+            Assert.That(checkFile, Is.False, $"{Config.ExchangeSetSerialEncFile} File should not exist in the specified folder path : {DownloadedFolderPath}");
+        }
+
+        [OneTimeTearDown]
+        public void GlobalTeardown()
+        {
+            //Clean up downloaded files/folders   
+            FileContentHelper.DeleteDirectory(Config.AIOConfig.AioExchangeSetFileName);
+        }
+    }
+}
