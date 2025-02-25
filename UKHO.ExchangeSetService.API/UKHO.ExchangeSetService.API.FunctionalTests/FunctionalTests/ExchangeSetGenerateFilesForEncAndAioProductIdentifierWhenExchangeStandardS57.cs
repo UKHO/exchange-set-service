abd9a085ad20aeeb3fa5f89ea958ecb7eb9bb9ef
JiviteshT <112479261+JiviteshT@users.noreply.github.com>
2025-02-25 11:11:35 +0000

    Dev/abzu 205266 s57 remove serial enc (#443)
    * Add conditional check for CreateSerialEncFile method
    
    The code modification ensures that the CreateSerialEncFile method is invoked only when the encryption parameter is true. This is achieved by wrapping the method call in an if statement that checks the value of encryption. Additionally, a new line is added to call DownloadReadMeFileAsync after the conditional check. The rest of the code remains unchanged, maintaining the existing functionality of creating product files and catalog files.
    
    * Abzu-205266 Change to create serial.enc only for S-63 (encrypted exs)
    
    * Updated unit test
    
    * Add functional tests for AIO file presence in exchange sets
    
    Introduced ExchangeSetGenerateFilesForEncAndAioProductVersionWhenExchangeStandardS57 class in UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests namespace. This class inherits from ObjectStorage and includes setup and teardown methods for functional tests. Added two test methods to verify the presence and absence of a specific serial AIO file in downloaded exchange sets. SetupAsync method initializes configurations, clients, and tokens, and prepares product version data. GlobalTeardown method cleans up downloaded files and folders.
    
    * Add async setup and new tests for AIO file checks
    
    Introduce System.Threading.Tasks in ExchangeSetGenerateFilesForEncAndAioProductVersionWhenExchangeStandardS57.cs.
    Add [TestFixture] attribute to the test class.
    Define async SetupAsync method with [OneTimeSetUp] attribute.
    Add two new test methods with [Category("QCOnlyTest-AIOEnabled")]:
    - WhenIDownloadAioZipExchangeSet_ThenASerialAioFileIsAvailable
    - WhenIDownloadZipExchangeSet_ThenASerialAioFileIsNotAvailable
    
    * Fix error message and rename method for file checks
    
    Updated `WhenIDownloadAioZipExchangeSet_ThenASerialAioFileIsAvailable` to correct the error message placeholder. Renamed `WhenIDownloadZipExchangeSet_ThenASerialAioFileIsNotAvailable` to `WhenIDownloadZipExchangeSet_ThenASerialFileIsNotAvailable` and updated file existence check within the renamed method.
    
    * Refactor and add tests for ExchangeSet functionality
    
    Refactored ExchangeSetGenerateFilesForEncAndAioProductVersionWhenExchangeStandardS57.cs by removing unused imports and unnecessary client initializations. Introduced a new test class in ExchangeSetGenerateFilesForEncAndAioProductIdentifierWhenExchangeStandardS57.cs with setup and teardown methods, and added tests to verify the presence or absence of specific files in the downloaded AIO zip exchange set.
    
    * Update assertion messages for clarity in test methods
    
    Updated assertion messages in `ExchangeSetGenerateFilesForEncAndAioProductIdentifierWhenExchangeStandardS57.cs` and `ExchangeSetGenerateFilesForEncAndAioProductVersionWhenExchangeStandardS57.cs` to "File should exist in the specified folder path" for better clarity and grammatical correctness.

---
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API.FunctionalTests/FunctionalTests/ExchangeSetGenerateFilesForEncAndAioProductIdentifierWhenExchangeStandardS57.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using UKHO.ExchangeSetService.API.FunctionalTests.Helper;
+
+namespace UKHO.ExchangeSetService.API.FunctionalTests.FunctionalTests
+{
+    [TestFixture]
+    public class ExchangeSetGenerateFilesForEncAndAioProductIdentifierWhenExchangeStandardS57 : ObjectStorage
+    {
+        [OneTimeSetUp]
+        public async Task SetupAsync()
+        {
+            Config = new TestConfiguration();
+            ExchangeSetApiClient = new ExchangeSetApiClient(Config.EssBaseAddress);
+            AuthTokenProvider authTokenProvider = new AuthTokenProvider();
+            EssJwtToken = await authTokenProvider.GetEssToken();
+            FssJwtToken = await authTokenProvider.GetFssToken();
+            DataHelper = new DataHelper();
+            ApiEssResponse = await ExchangeSetApiClient.GetProductIdentifiersDataAsync(DataHelper.GetProductIdentifiersForEncAndAio(), accessToken: EssJwtToken, callbackUri: null, exchangeSetStandard: "s57");
+            DownloadedFolderPath = await FileContentHelper.DownloadAndExtractAioZip(ApiEssResponse, FssJwtToken);
+        }
+
+        
+        [Test]
+        [Category("QCOnlyTest-AIOEnabled")]
+        public void WhenIDownloadAioZipExchangeSet_ThenASerialAioFileIsAvailable()
+        {
+            bool checkFile = FssBatchHelper.CheckforFileExist(DownloadedFolderPath, Config.AIOConfig.ExchangeSetSerialAioFile);
+            Assert.That(checkFile, Is.True, $"{Config.AIOConfig.ExchangeSetSerialAioFile} File should exist in the specified folder path : {DownloadedFolderPath}");
+        }
+
+        [Test]
+        [Category("QCOnlyTest-AIOEnabled")]
+        public void WhenIDownloadZipExchangeSet_ThenASerialFileIsNotAvailable()
+        {
+            bool checkFile = FssBatchHelper.CheckforFileExist(DownloadedFolderPath, Config.ExchangeSetSerialEncFile);
+            Assert.That(checkFile, Is.False, $"{Config.ExchangeSetSerialEncFile} File should not exist in the specified folder path : {DownloadedFolderPath}");
+        }
+
+        [OneTimeTearDown]
+        public void GlobalTeardown()
+        {
+            //Clean up downloaded files/folders   
+            FileContentHelper.DeleteDirectory(Config.AIOConfig.AioExchangeSetFileName);
+        }
+    }
+}
