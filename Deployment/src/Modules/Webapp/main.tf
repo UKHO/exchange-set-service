e8dbfeff2790fe6502120a3f6934031a2574beae
HugoBurgess <hugo.burgess@ukho.gov.uk>
2025-03-11 13:59:29 +0000

    Correct resource reference

--- Deployment/src/Modules/Webapp/main.tf
+++ Deployment/src/Modules/Webapp/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_app_service" "webapp_service" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+  app_service_plan_id = azurerm_service_plan.app_service_plan.id
   tags                = var.tags
 
   site_config {
