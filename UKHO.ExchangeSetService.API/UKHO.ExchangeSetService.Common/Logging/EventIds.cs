78984cd3ee8cad0f938ad6b20a64ad23369f11ed
hukho <128484816+hukho@users.noreply.github.com>
2025-11-12 11:17:45 +0000

    Add new EventIds

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Logging/EventIds.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.Common/Logging/EventIds.cs
@@ -4,7 +4,7 @@ namespace UKHO.ExchangeSetService.Common.Logging
 {
     public enum EventIds
     {
-        /*Event id range for ESS - 805000 to 805122 */
+        /*Event id range for ESS - 805000 to 805217 */
         /// <summary>
         /// 805000 - Request for sales catalogue service products sincedatetime endpoint is started.
         /// </summary>
@@ -852,7 +852,27 @@ namespace UKHO.ExchangeSetService.Common.Logging
         /// <summary>
         /// 805213 - serial.enc file creation is skipped
         /// </summary>
-        SerialFileCreationSkipped = 805212
+        SerialFileCreationSkipped = 805212,
+        /// <summary>
+        /// 805213 - Request for deleting temporary folders and files of exchange set is started.
+        /// </summary>
+        FulfilmentBatchCleanUpStarted = 805213,
+        /// <summary>
+        /// 805214 - Request for deleting temporary folders and files of exchange set is completed.
+        /// </summary>
+        FulfilmentBatchCleanUpCompleted = 805214,
+        /// <summary>
+        /// 805215 - Request for deleting temporary folders and files of exchange set is failed.
+        /// </summary>
+        FulfilmentBatchCleanUpFailed = 805215,
+        /// <summary>
+        /// 805216 - Fulfilment batch temporary folder is deleted successfully.
+        /// </summary>
+        FulfilmentBatchTemporaryFolderDeleted = 805216,
+        /// <summary>
+        /// 805217 - Fulfilment batch temporary folder is not found.
+        /// </summary>
+        FulfilmentBatchTemporaryFolderNotFound = 805217
     }
 
     public static class EventIdExtensions
