6412cea85825cf06c7839ae43335e0f7c252b9b8
hukho <128484816+hukho@users.noreply.github.com>
2026-03-24 12:24:21 +0000

    Remove unused code

--- MockApiDeployment/src/Modules/Webapp/main.tf
+++ MockApiDeployment/src/Modules/Webapp/main.tf
@@ -7,25 +7,6 @@ resource "azurerm_service_plan" "app_service_plan" {
   tags                   = var.tags
 }
 
-#resource "azurerm_app_service" "fulfillment_webapp" {
-#  name                = "${var.service_name}-${var.env_name}-fulfillment-${var.unique_string}-webapp"
-#  location            = var.location
-#  resource_group_name = var.resource_group_name
-#  app_service_plan_id = azurerm_service_plan.app_service_plan.id
-#  tags                = var.tags
-#  site_config {
-#    windows_fx_version  =   "DOTNETCORE|6.0"
-#    
-#    always_on  = true
-#    ftps_state = "Disabled"
-#  }
-#  app_settings = var.app_settings2
-#  identity {
-#    type = "UserAssigned"
-#    identity_ids = [var.user_assigned_identity]
-#  }
-#}
-
 resource "azurerm_windows_web_app" "fulfillment_webapp" {
   name                      = "${var.service_name}-${var.env_name}-fulfillment-${var.unique_string}-webapp"
   location                  = var.location
@@ -52,29 +33,6 @@ resource "azurerm_windows_web_app" "fulfillment_webapp" {
   }
 }
 
-#resource "azurerm_app_service" "scs_fss_mock_webapp" {
-#  name                = "${var.service_name}-${var.env_name}-mock-${var.unique_string}-webapp"
-#  location            = var.location
-#  resource_group_name = var.resource_group_name
-#  app_service_plan_id = azurerm_service_plan.app_service_plan.id
-#  tags                = var.tags
-#
-#  site_config {
-#    windows_fx_version  =   "DOTNETCORE|6.0"
-#    
-#    always_on  = true
-#    ftps_state = "Disabled"
-#  }
-#
-#  app_settings = var.app_settings
-#
-#  identity {
-#    type = "UserAssigned"
-#    identity_ids = [var.user_assigned_identity]
-#  }
-#
-#}
-
 resource "azurerm_windows_web_app" "scs_fss_mock_webapp" {
   name                      = "${var.service_name}-${var.env_name}-mock-${var.unique_string}-webapp"
   location                  = var.location
@@ -100,27 +58,6 @@ resource "azurerm_windows_web_app" "scs_fss_mock_webapp" {
     identity_ids = [var.user_assigned_identity]
   }
 }
-
-#resource "azurerm_app_service" "ess_webapp" {
-#  name                = "${var.service_name}-${var.env_name}-${var.unique_string}-webapp"
-#  location            = var.location
-#  resource_group_name = var.resource_group_name
-#  app_service_plan_id = azurerm_service_plan.app_service_plan.id
-#  tags                = var.tags
-#
-#  site_config {
-#    windows_fx_version  =   "DOTNETCORE|6.0"
-#    
-#    always_on  = true
-#    ftps_state = "Disabled"
-#  }
-
-#  identity {
-#    type = "UserAssigned"
-#    identity_ids = [var.user_assigned_identity]
-#  }
-#
-#}
 
 resource "azurerm_windows_web_app" "ess_webapp" {
   name                      = "${var.service_name}-${var.env_name}-${var.unique_string}-webapp"
