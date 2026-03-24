59d0a8b12e712e8598df49db880bc891d8bfe7f5
hukho <128484816+hukho@users.noreply.github.com>
2026-03-24 10:39:08 +0000

    Update outputs

--- MockApiDeployment/src/Modules/Webapp/outputs.tf
+++ MockApiDeployment/src/Modules/Webapp/outputs.tf
@@ -1,36 +1,35 @@
 output "fulfillment_webapp" {
-  value = azurerm_app_service.fulfillment_webapp.name
+  value = azurerm_windows_web_app.fulfillment_webapp.name
 }
 
 output "web_app_object_id_fulfillment" {
-  value = azurerm_app_service.fulfillment_webapp.identity.0.principal_id
+  value = azurerm_windows_web_app.fulfillment_webapp.identity.0.principal_id
 }
 
 output "default_site_hostname_fulfillment" {
-  value = azurerm_app_service.fulfillment_webapp.default_site_hostname
+  value = azurerm_windows_web_app.fulfillment_webapp.default_site_hostname
 }
 
 output "scs_fss_mock_webapp" {
-  value = azurerm_app_service.scs_fss_mock_webapp.name
+  value = azurerm_windows_web_app.scs_fss_mock_webapp.name
 }
 
 output "web_app_object_id_scs_fss_mock" {
-  value = azurerm_app_service.scs_fss_mock_webapp.identity.0.principal_id
+  value = azurerm_windows_web_app.scs_fss_mock_webapp.identity.0.principal_id
 }
 
 output "default_site_hostname_scs_fss_mock" {
-  value = azurerm_app_service.scs_fss_mock_webapp.default_site_hostname
+  value = azurerm_windows_web_app.scs_fss_mock_webapp.default_site_hostname
 }
 
 output "ess_webapp" {
-  value = azurerm_app_service.ess_webapp.name
+  value = azurerm_windows_web_app.ess_webapp.name
 }
 
 output "web_app_object_id_ess" {
-  value = azurerm_app_service.ess_webapp.identity.0.principal_id
+  value = azurerm_windows_web_app.ess_webapp.identity.0.principal_id
 }
 
 output "default_site_hostname_ess" {
-  value = azurerm_app_service.ess_webapp.default_site_hostname
+  value = azurerm_windows_web_app.ess_webapp.default_site_hostname
 }
-
