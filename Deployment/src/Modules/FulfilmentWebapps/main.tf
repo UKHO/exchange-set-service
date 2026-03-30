b5ff7d50cb6f56866d164ccae31e8c257123514c
hukho <128484816+hukho@users.noreply.github.com>
2026-03-30 13:38:35 +0100

    Update Terraform resources for web apps (#523)
    * Update main webapp
    
    * Fix outputs
    
    * Migration for fulfilment
    
    * Fix resource reference
    
    * Further fix
    
    * Import staging
    
    * Amend Terraform to medium and large jobs
    
    * Remove temp changes

--- Deployment/src/Modules/FulfilmentWebapps/main.tf
+++ Deployment/src/Modules/FulfilmentWebapps/main.tf
@@ -14,23 +14,29 @@ resource "azurerm_service_plan" "small_exchange_set_app_service_plan" {
   zone_balancing_enabled = var.asp_control_sxs.zoneRedundant
 }
 
-resource "azurerm_app_service" "small_exchange_set_webapp" {
+resource "azurerm_windows_web_app" "small_exchange_set_webapp" {
   lifecycle {
     replace_triggered_by = [terraform_data.replacement_sxs]
   }
 
-  count               = var.exchange_set_config.SmallExchangeSetInstance
-  name                = var.as_name_sxs[count.index]
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_service_plan.small_exchange_set_app_service_plan[count.index].id
-  tags                = var.tags
+  count                     = var.exchange_set_config.SmallExchangeSetInstance
+  name                      = var.as_name_sxs[count.index]
+  location                  = var.location
+  resource_group_name       = var.resource_group_name
+  service_plan_id           = azurerm_service_plan.small_exchange_set_app_service_plan[count.index].id
+  tags                      = var.tags
+  virtual_network_subnet_id = var.small_exchange_set_subnets[count.index]
 
   site_config {
-    windows_fx_version  =   "DOTNETCORE|6.0"
+    application_stack {
+      current_stack = "dotnet"
+      dotnet_version = "v8.0"
+    }
     
-    always_on  = true
-    ftps_state = "Disabled"
+    always_on         = true
+    ftps_state        = "Disabled"
+    use_32_bit_worker = false
+    
     ip_restriction {
       virtual_network_subnet_id = var.small_exchange_set_subnets[count.index]
     }
@@ -38,6 +44,10 @@ resource "azurerm_app_service" "small_exchange_set_webapp" {
 
   app_settings = var.app_settings
 
+  sticky_settings {
+    app_setting_names = [ "WEBJOBS_STOPPED" ]
+  }
+
   identity {
     type = "UserAssigned"
     identity_ids = [var.user_assigned_identity]
@@ -46,50 +56,40 @@ resource "azurerm_app_service" "small_exchange_set_webapp" {
   https_only = true
 }
 
-resource "azurerm_app_service_slot" "small_exchange_set_staging" {
+resource "azurerm_windows_web_app_slot" "small_exchange_set_staging" {
   lifecycle {
     replace_triggered_by = [terraform_data.replacement_sxs]
   }
 
-  count               = var.exchange_set_config.SmallExchangeSetInstance
-  name                = "staging"
-  app_service_name    = azurerm_app_service.small_exchange_set_webapp[count.index].name
-  location            = azurerm_app_service.small_exchange_set_webapp[count.index].location
-  resource_group_name = azurerm_app_service.small_exchange_set_webapp[count.index].resource_group_name
-  app_service_plan_id = azurerm_service_plan.small_exchange_set_app_service_plan[count.index].id
-  tags                = azurerm_app_service.small_exchange_set_webapp[count.index].tags
+  count                     = var.exchange_set_config.SmallExchangeSetInstance
+  name                      = "staging"
+  app_service_id            = azurerm_windows_web_app.small_exchange_set_webapp[count.index].id
+  tags                      = azurerm_windows_web_app.small_exchange_set_webapp[count.index].tags
+  virtual_network_subnet_id = var.small_exchange_set_subnets[count.index]
 
   site_config {
-    windows_fx_version  =   "DOTNETCORE|6.0"
+    application_stack {
+      current_stack = "dotnet"
+      dotnet_version = "v8.0"
+    }
     
-    always_on  = true
-    ftps_state = "Disabled"
+    always_on         = true
+    ftps_state        = "Disabled"
+    use_32_bit_worker = false
+
     ip_restriction {
       virtual_network_subnet_id = var.small_exchange_set_subnets[count.index]
     }
   }
 
-  app_settings = merge(azurerm_app_service.small_exchange_set_webapp[count.index].app_settings, {"WEBJOBS_STOPPED"="1"})
+  app_settings = merge(azurerm_windows_web_app.small_exchange_set_webapp[count.index].app_settings, { "WEBJOBS_STOPPED" = "1" })
 
   identity {
     type = "UserAssigned"
     identity_ids = [var.user_assigned_identity]
   }
 
-  https_only = true
-}
-
-resource "azurerm_app_service_virtual_network_swift_connection" "small_exchange_set_webapp_vnet_integration" {
-  count = var.exchange_set_config.SmallExchangeSetInstance
-  app_service_id = azurerm_app_service.small_exchange_set_webapp[count.index].id
-  subnet_id      = var.small_exchange_set_subnets[count.index]
-}
-
-resource "azurerm_app_service_slot_virtual_network_swift_connection" "small_exchange_set_slot_vnet_integration" {
-  count = var.exchange_set_config.SmallExchangeSetInstance
-  app_service_id = azurerm_app_service.small_exchange_set_webapp[count.index].id
-  subnet_id      = var.small_exchange_set_subnets[count.index]
-  slot_name      = azurerm_app_service_slot.small_exchange_set_staging[count.index].name
+  https_only = azurerm_windows_web_app.small_exchange_set_webapp[count.index].https_only
 }
 
 # Medium exchange set
@@ -108,23 +108,29 @@ resource "azurerm_service_plan" "medium_exchange_set_app_service_plan" {
   zone_balancing_enabled = var.asp_control_mxs.zoneRedundant
 }
 
-resource "azurerm_app_service" "medium_exchange_set_webapp" {
+resource "azurerm_windows_web_app" "medium_exchange_set_webapp" {
   lifecycle {
     replace_triggered_by = [terraform_data.replacement_mxs]
   }
 
-  count               = var.exchange_set_config.MediumExchangeSetInstance
-  name                = var.as_name_mxs[count.index]
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_service_plan.medium_exchange_set_app_service_plan[count.index].id
-  tags                = var.tags
+  count                     = var.exchange_set_config.MediumExchangeSetInstance
+  name                      = var.as_name_mxs[count.index]
+  location                  = var.location
+  resource_group_name       = var.resource_group_name
+  service_plan_id           = azurerm_service_plan.medium_exchange_set_app_service_plan[count.index].id
+  tags                      = var.tags
+  virtual_network_subnet_id = var.medium_exchange_set_subnets[count.index]
 
   site_config {
-    windows_fx_version  =   "DOTNETCORE|6.0"
+    application_stack {
+      current_stack = "dotnet"
+      dotnet_version = "v8.0"
+    }
     
-    always_on  = true
-    ftps_state = "Disabled"
+    always_on         = true
+    ftps_state        = "Disabled"
+    use_32_bit_worker = false
+    
     ip_restriction {
       virtual_network_subnet_id = var.medium_exchange_set_subnets[count.index]
     }
@@ -132,6 +138,10 @@ resource "azurerm_app_service" "medium_exchange_set_webapp" {
 
   app_settings = var.app_settings
 
+  sticky_settings {
+    app_setting_names = [ "WEBJOBS_STOPPED" ]
+  }
+
   identity {
     type = "UserAssigned"
     identity_ids = [var.user_assigned_identity]
@@ -140,50 +150,40 @@ resource "azurerm_app_service" "medium_exchange_set_webapp" {
   https_only = true
 }
 
-resource "azurerm_app_service_slot" "medium_exchange_set_staging" {
+resource "azurerm_windows_web_app_slot" "medium_exchange_set_staging" {
   lifecycle {
     replace_triggered_by = [terraform_data.replacement_mxs]
   }
 
-  count               = var.exchange_set_config.MediumExchangeSetInstance
-  name                = "staging"
-  app_service_name    = azurerm_app_service.medium_exchange_set_webapp[count.index].name
-  location            = azurerm_app_service.medium_exchange_set_webapp[count.index].location
-  resource_group_name = azurerm_app_service.medium_exchange_set_webapp[count.index].resource_group_name
-  app_service_plan_id = azurerm_service_plan.medium_exchange_set_app_service_plan[count.index].id
-  tags                = azurerm_app_service.medium_exchange_set_webapp[count.index].tags
+  count                     = var.exchange_set_config.MediumExchangeSetInstance
+  name                      = "staging"
+  app_service_id            = azurerm_windows_web_app.medium_exchange_set_webapp[count.index].id
+  tags                      = azurerm_windows_web_app.medium_exchange_set_webapp[count.index].tags
+  virtual_network_subnet_id = var.medium_exchange_set_subnets[count.index]
 
   site_config {
-    windows_fx_version  =   "DOTNETCORE|6.0"
+    application_stack {
+      current_stack = "dotnet"
+      dotnet_version = "v8.0"
+    }
     
-    always_on  = true
-    ftps_state = "Disabled"
+    always_on         = true
+    ftps_state        = "Disabled"
+    use_32_bit_worker = false
+
     ip_restriction {
       virtual_network_subnet_id = var.medium_exchange_set_subnets[count.index]
     }
   }
 
-  app_settings = merge(azurerm_app_service.medium_exchange_set_webapp[count.index].app_settings, {"WEBJOBS_STOPPED"="1"})
+  app_settings = merge(azurerm_windows_web_app.medium_exchange_set_webapp[count.index].app_settings, { "WEBJOBS_STOPPED" = "1" })
 
   identity {
     type = "UserAssigned"
     identity_ids = [var.user_assigned_identity]
   }
 
-  https_only = true
-}
-
-resource "azurerm_app_service_virtual_network_swift_connection" "medium_exchange_set_webapp_vnet_integration" {
-  count = var.exchange_set_config.MediumExchangeSetInstance
-  app_service_id = azurerm_app_service.medium_exchange_set_webapp[count.index].id
-  subnet_id      = var.medium_exchange_set_subnets[count.index]
-}
-
-resource "azurerm_app_service_slot_virtual_network_swift_connection" "medium_exchange_set_slot_vnet_integration" {
-  count = var.exchange_set_config.MediumExchangeSetInstance
-  app_service_id = azurerm_app_service.medium_exchange_set_webapp[count.index].id
-  subnet_id      = var.medium_exchange_set_subnets[count.index]
-  slot_name      = azurerm_app_service_slot.medium_exchange_set_staging[count.index].name
+  https_only = azurerm_windows_web_app.medium_exchange_set_webapp[count.index].https_only
 }
 
 # Large exchange set
@@ -202,23 +202,29 @@ resource "azurerm_service_plan" "large_exchange_set_app_service_plan" {
   zone_balancing_enabled = var.asp_control_lxs.zoneRedundant
 }
 
-resource "azurerm_app_service" "large_exchange_set_webapp" {
+resource "azurerm_windows_web_app" "large_exchange_set_webapp" {
   lifecycle {
     replace_triggered_by = [terraform_data.replacement_lxs]
   }
 
-  count               = var.exchange_set_config.LargeExchangeSetInstance
-  name                = var.as_name_lxs[count.index]
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  app_service_plan_id = azurerm_service_plan.large_exchange_set_app_service_plan[count.index].id
-  tags                = var.tags
+  count                     = var.exchange_set_config.LargeExchangeSetInstance
+  name                      = var.as_name_lxs[count.index]
+  location                  = var.location
+  resource_group_name       = var.resource_group_name
+  service_plan_id           = azurerm_service_plan.large_exchange_set_app_service_plan[count.index].id
+  tags                      = var.tags
+  virtual_network_subnet_id = var.large_exchange_set_subnets[count.index]
 
   site_config {
-    windows_fx_version  =   "DOTNETCORE|6.0"
+    application_stack {
+      current_stack = "dotnet"
+      dotnet_version = "v8.0"
+    }
     
-    always_on  = true
-    ftps_state = "Disabled"
+    always_on         = true
+    ftps_state        = "Disabled"
+    use_32_bit_worker = false
+    
     ip_restriction {
       virtual_network_subnet_id = var.large_exchange_set_subnets[count.index]
     }
@@ -226,6 +232,10 @@ resource "azurerm_app_service" "large_exchange_set_webapp" {
 
   app_settings = var.app_settings
 
+  sticky_settings {
+    app_setting_names = [ "WEBJOBS_STOPPED" ]
+  }
+
   identity {
     type = "UserAssigned"
     identity_ids = [var.user_assigned_identity]
@@ -234,48 +244,38 @@ resource "azurerm_app_service" "large_exchange_set_webapp" {
   https_only = true
 }
 
-resource "azurerm_app_service_slot" "large_exchange_set_staging" {
+resource "azurerm_windows_web_app_slot" "large_exchange_set_staging" {
   lifecycle {
     replace_triggered_by = [terraform_data.replacement_lxs]
   }
 
-  count               = var.exchange_set_config.LargeExchangeSetInstance
-  name                = "staging"
-  app_service_name    = azurerm_app_service.large_exchange_set_webapp[count.index].name
-  location            = azurerm_app_service.large_exchange_set_webapp[count.index].location
-  resource_group_name = azurerm_app_service.large_exchange_set_webapp[count.index].resource_group_name
-  app_service_plan_id = azurerm_service_plan.large_exchange_set_app_service_plan[count.index].id
-  tags                = azurerm_app_service.large_exchange_set_webapp[count.index].tags
+  count                     = var.exchange_set_config.LargeExchangeSetInstance
+  name                      = "staging"
+  app_service_id            = azurerm_windows_web_app.large_exchange_set_webapp[count.index].id
+  tags                      = azurerm_windows_web_app.large_exchange_set_webapp[count.index].tags
+  virtual_network_subnet_id = var.large_exchange_set_subnets[count.index]
 
   site_config {
-    windows_fx_version  =   "DOTNETCORE|6.0"
+    application_stack {
+      current_stack = "dotnet"
+      dotnet_version = "v8.0"
+    }
     
-    always_on  = true
-    ftps_state = "Disabled"
+    always_on         = true
+    ftps_state        = "Disabled"
+    use_32_bit_worker = false
+
     ip_restriction {
       virtual_network_subnet_id = var.large_exchange_set_subnets[count.index]
     }
   }
 
-  app_settings = merge(azurerm_app_service.large_exchange_set_webapp[count.index].app_settings, {"WEBJOBS_STOPPED"="1"})
+  app_settings = merge(azurerm_windows_web_app.large_exchange_set_webapp[count.index].app_settings, { "WEBJOBS_STOPPED" = "1" })
 
   identity {
     type = "UserAssigned"
     identity_ids = [var.user_assigned_identity]
   }
 
-  https_only = true
-}
-
-resource "azurerm_app_service_virtual_network_swift_connection" "large_exchange_set_webapp_vnet_integration" {
-  count = var.exchange_set_config.LargeExchangeSetInstance
-  app_service_id = azurerm_app_service.large_exchange_set_webapp[count.index].id
-  subnet_id      = var.large_exchange_set_subnets[count.index]
-}
-
-resource "azurerm_app_service_slot_virtual_network_swift_connection" "large_exchange_set_slot_vnet_integration" {
-  count = var.exchange_set_config.LargeExchangeSetInstance
-  app_service_id = azurerm_app_service.large_exchange_set_webapp[count.index].id
-  subnet_id      = var.large_exchange_set_subnets[count.index]
-  slot_name      = azurerm_app_service_slot.large_exchange_set_staging[count.index].name
+  https_only = azurerm_windows_web_app.large_exchange_set_webapp[count.index].https_only
 }
--- Deployment/src/migration.tf
+++ Deployment/src/migration.tf
@@ -15,3 +15,183 @@ import {
   to = module.webapp_service.azurerm_service_plan.app_service_plan
   id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Web/serverFarms/${local.web_app_name}-asp"
 }
+
+# For web apps below see PBI #159288.
+# Main webapp
+removed {
+  from = module.webapp_service.azurerm_app_service.webapp_service
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  to = module.webapp_service.azurerm_windows_web_app.webapp_service
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Web/sites/${local.web_app_name}"
+}
+
+removed {
+  from = module.webapp_service.azurerm_app_service_slot.staging
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  to = module.webapp_service.azurerm_windows_web_app_slot.staging
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Web/sites/${local.web_app_name}/slots/staging"
+}
+
+# swift
+removed {
+  from = module.webapp_service.azurerm_app_service_virtual_network_swift_connection.webapp_vnet_integration
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.webapp_service.azurerm_app_service_slot_virtual_network_swift_connection.slot_vnet_integration
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+# Fulfilment - SXS
+removed {
+  from = module.fulfilment_webapp.azurerm_app_service.small_exchange_set_webapp
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  for_each = range(local.config_data.ESSFulfilmentConfiguration.SmallExchangeSetInstance)
+  to = module.fulfilment_webapp.azurerm_windows_web_app.small_exchange_set_webapp[each.key]
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Web/sites/${local.as_name_sxs[each.key]}"
+}
+
+removed {
+  from = module.fulfilment_webapp.azurerm_app_service_slot.small_exchange_set_staging
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  for_each = range(local.config_data.ESSFulfilmentConfiguration.SmallExchangeSetInstance)
+  to = module.fulfilment_webapp.azurerm_windows_web_app_slot.small_exchange_set_staging[each.key]
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Web/sites/${local.as_name_sxs[each.key]}/slots/staging"
+}
+
+removed {
+  from = module.fulfilment_webapp.azurerm_app_service_virtual_network_swift_connection.small_exchange_set_webapp_vnet_integration
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.fulfilment_webapp.azurerm_app_service_slot_virtual_network_swift_connection.small_exchange_set_slot_vnet_integration
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+# Fulfilment - MXS
+removed {
+  from = module.fulfilment_webapp.azurerm_app_service.medium_exchange_set_webapp
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  for_each = range(local.config_data.ESSFulfilmentConfiguration.MediumExchangeSetInstance)
+  to = module.fulfilment_webapp.azurerm_windows_web_app.medium_exchange_set_webapp[each.key]
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Web/sites/${local.as_name_mxs[each.key]}"
+}
+
+removed {
+  from = module.fulfilment_webapp.azurerm_app_service_slot.medium_exchange_set_staging
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  for_each = range(local.config_data.ESSFulfilmentConfiguration.MediumExchangeSetInstance)
+  to = module.fulfilment_webapp.azurerm_windows_web_app_slot.medium_exchange_set_staging[each.key]
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Web/sites/${local.as_name_mxs[each.key]}/slots/staging"
+}
+
+removed {
+  from = module.fulfilment_webapp.azurerm_app_service_virtual_network_swift_connection.medium_exchange_set_webapp_vnet_integration
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.fulfilment_webapp.azurerm_app_service_slot_virtual_network_swift_connection.medium_exchange_set_slot_vnet_integration
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+# Fulfilment - LXS
+removed {
+  from = module.fulfilment_webapp.azurerm_app_service.large_exchange_set_webapp
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  for_each = range(local.config_data.ESSFulfilmentConfiguration.LargeExchangeSetInstance)
+  to = module.fulfilment_webapp.azurerm_windows_web_app.large_exchange_set_webapp[each.key]
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Web/sites/${local.as_name_lxs[each.key]}"
+}
+
+removed {
+  from = module.fulfilment_webapp.azurerm_app_service_slot.large_exchange_set_staging
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  for_each = range(local.config_data.ESSFulfilmentConfiguration.LargeExchangeSetInstance)
+  to = module.fulfilment_webapp.azurerm_windows_web_app_slot.large_exchange_set_staging[each.key]
+  id = "${azurerm_resource_group.rg.id}/providers/Microsoft.Web/sites/${local.as_name_lxs[each.key]}/slots/staging"
+}
+
+removed {
+  from = module.fulfilment_webapp.azurerm_app_service_virtual_network_swift_connection.large_exchange_set_webapp_vnet_integration
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = module.fulfilment_webapp.azurerm_app_service_slot_virtual_network_swift_connection.large_exchange_set_slot_vnet_integration
+
+  lifecycle {
+    destroy = false
+  }
+}
