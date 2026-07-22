b7d7ac6b3b4ab75d382e0b87eb1e0f87f2ec0d84
Ganpat Gawde <Ganpat14802@Mastek.Com>
2026-07-22 17:11:22 +0530

    Add -upgrade flag to terraform init in deployment scripts
    Updated terraform init commands in three PowerShell deployment scripts to include the -upgrade flag. This change ensures that Terraform modules and providers are upgraded to the latest acceptable versions during initialization. No other logic was altered.

--- Deployment/terraform_conditional_run_v2.ps1
+++ Deployment/terraform_conditional_run_v2.ps1
@@ -13,7 +13,7 @@ cd $env:AGENT_BUILDDIRECTORY/terraformartifact/src
 terraform --version
 
 Write-output "Executing terraform scripts for deployment in $workSpace enviroment"
-terraform init -backend-config="resource_group_name=$deploymentResourceGroupName" -backend-config="storage_account_name=$deploymentStorageAccountName" -backend-config="key=terraform.deployment.v2.tfplan"
+terraform init -upgrade -backend-config="resource_group_name=$deploymentResourceGroupName" -backend-config="storage_account_name=$deploymentStorageAccountName" -backend-config="key=terraform.deployment.v2.tfplan"
 if ( !$? ) { echo "Something went wrong during terraform initialization"; throw "Error" }
 
 Write-output "Selecting workspace"
