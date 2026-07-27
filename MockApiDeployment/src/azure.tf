192066fcdba23b9f4cfde0f11981afee4e1d2c00
Muyiwa Ayodele <muyiwa163989@mastek.com>
2026-07-27 22:02:37 +0100

    chore(terraform): bump terraform/azurerm versions

--- MockApiDeployment/src/azure.tf
+++ MockApiDeployment/src/azure.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.116.0"
+      version = "4.54.0"
     }
   }
 
@@ -16,3 +16,4 @@ terraform {
 provider "azurerm" {
   features { }
 }
+
