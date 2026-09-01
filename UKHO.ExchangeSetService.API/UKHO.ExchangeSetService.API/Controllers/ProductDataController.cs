3ca621d0c8b7112d7b464013323d7e67cd69584b
Ganpat Gawde <Ganpat14802@Mastek.Com>
2026-09-01 19:38:16 +0530

    Downgrade App Insights packages and clean up usings
    Downgraded Microsoft.ApplicationInsights and Microsoft.ApplicationInsights.AspNetCore package versions in test projects from 3.1.2 to 2.23.0. Removed unused using directives from ProductBuilderController, ProductDataController, and ProductInformationController for improved code clarity.

--- UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/ProductDataController.cs
+++ UKHO.ExchangeSetService.API/UKHO.ExchangeSetService.API/Controllers/ProductDataController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi;
 using Swashbuckle.AspNetCore.Annotations;
 using Swashbuckle.AspNetCore.Filters;
 using System.Collections.Generic;
